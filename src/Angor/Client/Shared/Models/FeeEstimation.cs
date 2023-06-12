fbeef194a5065e9460baf48a34e1c6e0b982a5d3
TheDude <david@vsodir.onmicrosoft.com>
2023-06-12 10:45:58 +0100

    Added basic call to get the fee and fixed 1000 in case of failure

---
+++ src/Angor/Client/Shared/Models/FeeEstimation.cs
@@ -0,0 +1,7 @@
+namespace Angor.Client.Shared.Models;
+
+public class FeeEstimation
+{
+    public int Confirmations { get; set; }
+    public long FeeRateet { get; set; }
+}
---
+++ src/Angor/Client/Shared/Models/FeeEstimations.cs
@@ -0,0 +1,6 @@
+namespace Angor.Client.Shared.Models;
+
+public class FeeEstimations
+{
+    public List<FeeEstimation>  Fees { get; set; }
+}
