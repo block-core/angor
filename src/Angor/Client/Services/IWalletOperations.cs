a41f80058559df1648c69be09c28cf81bb287546
TheDude <david@vsodir.onmicrosoft.com>
2023-06-13 12:10:08 +0100

    Started work on moving the loading of the wallet to a service

--- src/Angor/Client/Services/IWalletOperations.cs
+++ src/Angor/Client/Services/IWalletOperations.cs
@@ -1,6 +1,11 @@
+using Angor.Client.Shared.Models;
+
 namespace Angor.Client.Services;
 
 public interface IWalletOperations
 {
     Task<(bool, string)> SendAmountToAddress(decimal sendAmount, long selectedFee, string sendToAddress);
+    void BuildAccountInfoForWalletWords();
+    Task UpdateAccountInfo();
+    Task<(bool noHistory, List<UtxoData> data)> FetchUtxos(string adddress);
 }
--- src/Angor/Client/Services/WalletOperations.cs
+++ src/Angor/Client/Services/WalletOperations.cs
@@ -1,4 +1,6 @@
+using System.Net.Http.Json;
 using Angor.Client.Shared.Models;
+using Angor.Client.Shared.Types;
 using Angor.Shared;
 using Blockcore.Consensus.ScriptInfo;
 using Blockcore.Consensus.TransactionInfo;
@@ -113,5 +115,231 @@ public class WalletOperations : IWalletOperations
         var content = await res.Content.ReadAsStringAsync();
         
         return (false, res.ReasonPhrase + content);
+    }
+
+    public void BuildAccountInfoForWalletWords()
+    {
+        ExtKey.UseBCForHMACSHA512 = true;
+        Blockcore.NBitcoin.Crypto.Hashes.UseBCForHMACSHA512 = true;
+
+        Network network = _networkConfiguration.GetNetwork();
+        var coinType = network.Consensus.CoinType;
+        var accountIndex = 0; // for now only account 0
+        var purpose = 84; // for now only legacy
+
+        AccountInfo accountInfo = _storage.GetAccountInfo(network.Name);
+
+        if (accountInfo != null)
+            return;
+
+        accountInfo = new AccountInfo();
+
+        ExtKey extendedKey;
+        try
+        {
+            extendedKey = _hdOperations.GetExtendedKey(_storage.GetWalletWords());
+        }
+        catch (NotSupportedException ex)
+        {
+            Console.WriteLine("Exception occurred: {0}", ex.ToString());
+
+            if (ex.Message == "Unknown")
+                throw new Exception("Please make sure you enter valid mnemonic words.");
+
+            throw;
+        }
+
+        string accountHdPath = _hdOperations.GetAccountHdPath(purpose, coinType, accountIndex);
+        Key privateKey = extendedKey.PrivateKey;
+        _storage.SetWalletPubkey(privateKey.PubKey.ToHex());
+        //storage.SetWalletPrivkey(extendedKey.ToString(network)!);
+        ExtPubKey accountExtPubKeyTostore =
+            _hdOperations.GetExtendedPublicKey(privateKey, extendedKey.ChainCode, accountHdPath);
+
+        accountInfo.ExtPubKey = accountExtPubKeyTostore.ToString(network);
+        accountInfo.Path = accountHdPath;
+
+        _storage.SetAccountInfo(network.Name, accountInfo);
+    }
+
+    public async Task UpdateAccountInfo()
+    {
+        ExtKey.UseBCForHMACSHA512 = true;
+        Blockcore.NBitcoin.Crypto.Hashes.UseBCForHMACSHA512 = true;
+
+        Network network = _networkConfiguration.GetNetwork();
+
+        AccountInfo accountInfo = _storage.GetAccountInfo(network.Name);
+
+        var (index, items) = await FetchUtxoForAddressAsync(accountInfo.LastFetchIndex, accountInfo.ExtPubKey, network, false);
+
+        accountInfo.LastFetchIndex = index;
+        foreach (var (address, utxoData) in items)
+        {
+            accountInfo.UtxoItems.Remove(address);
+            accountInfo.UtxoItems.Add(address, utxoData);
+            accountInfo.TotalBalance += utxoData.Sum(s => s.value);
+        }
+        
+        var (changeIndex, changeItems) = await FetchUtxoForAddressAsync(accountInfo.LastFetchChangeIndex, accountInfo.ExtPubKey, network, true);
+
+        accountInfo.LastFetchChangeIndex = changeIndex;
+        foreach (var (address, utxoData) in changeItems)
+        {
+            accountInfo.UtxoChangeItems.Remove(address);
+            accountInfo.UtxoChangeItems.Add(address, utxoData);
+            accountInfo.TotalBalance += utxoData.Sum(s => s.value);
+        }
+
+        _storage.SetAccountInfo(network.Name, accountInfo);
+    }
+
+    private async Task CheckExistingAddresses(AccountInfo accountInfo) //TODO make this a public call
+    {
+        foreach (var item in accountInfo.UtxoItems)
+        {
+            if (item.Value.Any())
+            {
+                var result = await FetchUtxos(item.Key);
+
+                if (result.data.Count == item.Value.Count)
+                {
+                    for (int i = 0; i < result.data.Count - 1; i++)
+                    {
+                        if (result.data[i].outpoint.transactionId != item.Value[i].outpoint.transactionId)
+                        {
+                            result.data.ForEach(f => f.hdPath = item.Value[0].hdPath);
+
+                            item.Value.Clear();
+                            item.Value.AddRange(result.data);
+                            break;
+                        }
+
+                    }
+                }
+                else
+                {
+                    result.data.ForEach(f => f.hdPath = item.Value[0].hdPath);
+                    item.Value.Clear();
+                    item.Value.AddRange(result.data);
+                }
+            }
+        }
+
+        foreach (var item in accountInfo.UtxoChangeItems)
+        {
+            if (item.Value.Any())
+            {
+                var result = await FetchUtxos(item.Key);
+
+                if (result.data.Count == item.Value.Count)
+                {
+                    for (int i = 0; i < result.data.Count - 1; i++)
+                    {
+                        if (result.data[i].outpoint.transactionId != item.Value[i].outpoint.transactionId)
+                        {
+                            item.Value.Clear();
+                            item.Value.AddRange(result.data);
+                            break;
+                        }
+
+                    }
+                }
+                else
+                {
+                    item.Value.Clear();
+                    item.Value.AddRange(result.data);
+                }
+            }
+        }
+    }
+    
+    private async Task<(int,Dictionary<string,List<UtxoData>>)> FetchUtxoForAddressAsync(int scanIndex, string ExtendedPubKey, Network network, bool isChange)
+    {
+        ExtPubKey accountExtPubKey = ExtPubKey.Parse(ExtendedPubKey, network);
+        
+        var UtxoItems = new Dictionary<string,List<UtxoData>>();
+        var accountIndex = 0; // for now only account 0
+        var purpose = 84; // for now only legacy
+        
+        var gap = 5;
+        while (gap > 0)
+        {
+            PubKey pubkey = _hdOperations.GeneratePublicKey(accountExtPubKey, scanIndex, isChange);
+            var path = _hdOperations.CreateHdPath(purpose, network.Consensus.CoinType, accountIndex, isChange, scanIndex);
+            
+            var address = pubkey.GetSegwitAddress(network).ToString();
+            var result = await FetchUtxos(address);
+            
+            foreach (var utxoData in result.data)
+                utxoData.hdPath = path;
+            
+            UtxoItems.Add(address, result.data);
+            scanIndex++;
+
+            if (!result.noHistory) continue;
+            
+            gap--;
+        }
+
+        return (scanIndex, UtxoItems);
+    }
+
+    private async Task<(int, string, List<UtxoData>)> FetchUtxoForAddressAsync(int scanIndex,
+        ExtPubKey accountExtPubKey, Network network, bool isChange)
+    {
+        var UtxoItems = new Dictionary<string, List<UtxoData>>();
+        var accountIndex = 0; // for now only account 0
+        var purpose = 84; // for now only legacy
+
+        PubKey pubkey = _hdOperations.GeneratePublicKey(accountExtPubKey, scanIndex, isChange);
+        var path = _hdOperations.CreateHdPath(purpose, network.Consensus.CoinType, accountIndex, isChange, scanIndex);
+
+        var address = pubkey.GetSegwitAddress(network).ToString();
+        var result = await FetchUtxos(address);
+
+        foreach (var utxoData in result.data)
+            utxoData.hdPath = path;
+
+        return (scanIndex++, address, result.data);
+    }
+
+    public async Task<(bool noHistory, List<UtxoData> data)> FetchUtxos(string adddress)
+    {
+        var limit = 50;
+        var offset = 0;
+        List<UtxoData> allItems = new();
+
+        var urlBalance = $"/query/address/{adddress}";
+        IndexerUrl indexer = _networkConfiguration.getIndexerUrl();
+        var addressBalance = await _http.GetFromJsonAsync<AddressBalance>(indexer.Url + urlBalance);
+
+        if (addressBalance?.balance == 0 && (addressBalance.totalReceivedCount + addressBalance.totalSentCount) == 0)
+        {
+            return (true, allItems);
+        }
+
+        int fetchCount = 50; // for the demo we just scan 50 addresses
+
+        for (int i = 0; i < fetchCount; i++)
+        {
+            // this is inefficient look at headers to know when to stop
+
+            var url = $"/query/address/{adddress}/transactions/unspent?confirmations=0&offset={offset}&limit={limit}";
+
+            Console.WriteLine($"fetching {url}");
+
+            var response = await _http.GetAsync(indexer.Url + url);
+            var utxo = await response.Content.ReadFromJsonAsync<List<UtxoData>>();
+
+            if (utxo == null || !utxo.Any())
+                break;
+
+            allItems.AddRange(utxo);
+
+            offset += limit;
+        }
+
+        return (false, allItems);
     }
 }
