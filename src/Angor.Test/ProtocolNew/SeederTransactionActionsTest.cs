078e9399a6b4d97a3288a7381e3e5d75048e8f7a
TheDude <david@vsodir.onmicrosoft.com>
2023-08-15 13:25:40 +0100

    Added a test for the outputs script

--- src/Angor.Test/ProtocolNew/SeederTransactionActionsTest.cs
+++ src/Angor.Test/ProtocolNew/SeederTransactionActionsTest.cs
@@ -130,7 +130,7 @@ public class SeederTransactionActionsTest : AngorTestData
     }
 
     [Fact]
-    public void BuildRecoverSeederFundsTransactions_AllTransactionsReturnedValid()
+    public void BuildRecoverSeederFundsTransactions_AllTransactionsReturnedWithStageOutputssSpent()
     {
         var changeAddress = new Key();
         var investmentTrxHex = "010000000102df7eb0603e53da8760b6037cca974550e85489d36d59efcedb8834bb691a71000000006a473044022047870ec27da9e51c3f4657b6ac376bb240191b93b1c597775f092a3c6e20e66602203f544e972f45e796730c02942393f9b7d02b4d6dc2301281c68029e693194549012103d418fd52b6bd5fc9330c51aa4480aa9f3bba2940bedc7ce7535ac164aebc25efffffffff06c0c62d0000000000160014b81698f9e2e78fa7fc87f8009183c8a4ab25a6c70000000000000000446a2103eb7d47c80390672435987b9a7ecaa22730cd9c4537fc8d257417fb058248ed7720fcdcd57c6c65b40bcdf9b454a96891d7375a60d516e3416af61f86d4999d44e180c3c901000000002251204f3edc853deba516c82aa9479daaddbe5c34bdde1b2a7be369d784e560271123804a5d0500000000225120d2f4094dc5c80bee991b76b089f230f086edfcef20550467026f80e79647b3f900879303000000002251208ea1c42515559fd53f811b333be518484aeecf9409aefeccb8e977b43ae1d9c99c547e6d00000000160014333a905154f56ef18b6f7aee53ed45a231da54f700000000";
@@ -143,5 +143,32 @@ public class SeederTransactionActionsTest : AngorTestData
         //All inputs are from the investment transaction outputs
         Assert.Contains(recoveryTransactions.SelectMany(_ => _.Inputs.AsIndexedInputs()),
             _ => investmentTrx.Outputs.AsIndexedOutputs().Any(o => o.ToOutPoint().Equals(_.PrevOut)));
+    }
+    
+    [Fact]
+    public void BuildRecoverSeederFundsTransactions_AllTransactionsReturnedWithTheRightScript()
+    {
+        var changeAddress = new Key();
+        var investmentTrxHex = "010000000102df7eb0603e53da8760b6037cca974550e85489d36d59efcedb8834bb691a71000000006a473044022047870ec27da9e51c3f4657b6ac376bb240191b93b1c597775f092a3c6e20e66602203f544e972f45e796730c02942393f9b7d02b4d6dc2301281c68029e693194549012103d418fd52b6bd5fc9330c51aa4480aa9f3bba2940bedc7ce7535ac164aebc25efffffffff06c0c62d0000000000160014b81698f9e2e78fa7fc87f8009183c8a4ab25a6c70000000000000000446a2103eb7d47c80390672435987b9a7ecaa22730cd9c4537fc8d257417fb058248ed7720fcdcd57c6c65b40bcdf9b454a96891d7375a60d516e3416af61f86d4999d44e180c3c901000000002251204f3edc853deba516c82aa9479daaddbe5c34bdde1b2a7be369d784e560271123804a5d0500000000225120d2f4094dc5c80bee991b76b089f230f086edfcef20550467026f80e79647b3f900879303000000002251208ea1c42515559fd53f811b333be518484aeecf9409aefeccb8e977b43ae1d9c99c547e6d00000000160014333a905154f56ef18b6f7aee53ed45a231da54f700000000";
+        
+        var investmentTrx = Networks.Bitcoin.Testnet().Consensus.ConsensusFactory.CreateTransaction(investmentTrxHex);
+
+        var expectedAddress = Encoders.Hex.EncodeData(changeAddress.PubKey.ToBytes());
+        var expectedDateTime = DateTime.Now.AddMonths(6);
+        var expectedScript = new Key().ScriptPubKey;
+        
+        var mock = new Mock<IInvestmentScriptBuilder>();
+        mock.Setup(_ => _.GetInvestorPenaltyTransactionScript(expectedAddress, expectedDateTime))
+            .Returns(expectedScript);
+
+        _sut = new SeederTransactionActions(_networkConfiguration.Object,
+            mock.Object, _projectScriptsBuilder.Object);
+
+        var recoveryTransactions = _sut.BuildRecoverSeederFundsTransactions(investmentTrx, 
+            expectedDateTime, expectedAddress);
+        
+        //All outputs pay to the penalty script
+        Assert.Contains(recoveryTransactions.SelectMany(_ => _.Outputs).ToList(),
+            _ => _.ScriptPubKey.ToHex().Equals(expectedScript.WitHash.ScriptPubKey.ToHex()));
     }
 }
