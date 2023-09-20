48705a2a5ab02c0e699280f1fbc2589198f6d3ed
TheDude <david@vsodir.onmicrosoft.com>
2023-09-20 13:44:18 +0100

    Moved interface to a different file

--- src/Angor/Shared/DerivationOperations.cs
+++ src/Angor/Shared/DerivationOperations.cs
@@ -10,19 +10,6 @@ using System.IO;
 
 namespace Angor.Shared;
 
-public interface IDerivationOperations
-{
-    FounderKeyCollection DeriveProjectKeys(WalletWords walletWords, string angorTestKey);
-    FounderKeys GetProjectKey(FounderKeyCollection founderKeyCollection, int index);
-    string DeriveFounderKey(WalletWords walletWords, int index);
-    uint DeriveProjectId(string founderKey);
-    string DeriveAngorKey(string founderKey, string angorRootKey);
-    Script AngorKeyToScript(string angorKey);
-    string DeriveInvestorKey(WalletWords walletWords, string founderKey);
-    string DeriveSeederSecretHash(WalletWords walletWords, string founderKey);
-    Key DeriveFounderPrivateKey(WalletWords walletWords, int index);
-}
-
 public class DerivationOperations : IDerivationOperations
 {
     private readonly IHdOperations _hdOperations;
