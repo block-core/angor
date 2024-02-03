739ad759d572def5d65518dde6a12a34a3a31b98
dangershony <dan.gershony@gmail.com>
2024-02-03 12:31:50 +0000

    act on review

--- src/Angor/Shared/Models/Wallet.cs
+++ src/Angor/Shared/Models/Wallet.cs
@@ -1,34 +1,7 @@
-using System.Text.Json;
-
 namespace Angor.Shared.Models;
 public class Wallet
 {
     public WalletWords WalletWords { get; set; } // todo: this will go away when we add passwords
     public FounderKeyCollection FounderKeys { get; set; }
     public WalletPayload Payload { get; set; }
-}
-
-public class WalletWords
-{
-    public string Words { get; set; }
-    public string? Passphrase { get; set; }
-
-    public string ConvertToString()
-    {
-        return JsonSerializer.Serialize(this);
-    }
-
-    public static WalletWords ConvertFromString(string data)
-    {
-        if (string.IsNullOrEmpty(data))
-            throw new InvalidOperationException();
-
-        return JsonSerializer.Deserialize<WalletWords>(data) ?? throw new InvalidOperationException();
-    }
-}
-
-public class WalletPayload
-{
-    public string EncryptedData { get; set; }
-
 }
---
+++ src/Angor/Shared/Models/WalletWords.cs
@@ -0,0 +1,22 @@
+using System.Text.Json;
+
+namespace Angor.Shared.Models;
+
+public class WalletWords
+{
+    public string Words { get; set; }
+    public string? Passphrase { get; set; }
+
+    public string ConvertToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+
+    public static WalletWords ConvertFromString(string data)
+    {
+        if (string.IsNullOrEmpty(data))
+            throw new InvalidOperationException();
+
+        return JsonSerializer.Deserialize<WalletWords>(data) ?? throw new InvalidOperationException();
+    }
+}
