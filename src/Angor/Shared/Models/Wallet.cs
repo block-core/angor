f6951558514a22d34e9565a6374a8f163faa0f8a
Dan Gershony <dan.gershony@gmail.com>
2024-03-28 11:13:34 +0000

    Add password encryption (#49)
    * initial commit
    
    * Add wallet password
    
    * Add warnings
    
    * Fix WalletWordsClose method
    
    * Change password look
    
    * check password
    
    * add back the js crypto code
    
    * Creating a password component
    
    * password on create page
    
    * All components now use a password
    
    * Add password cache service
    
    * fix typo
    
    * act on review

--- src/Angor/Shared/Models/Wallet.cs
+++ src/Angor/Shared/Models/Wallet.cs
@@ -1,7 +1,6 @@
 namespace Angor.Shared.Models;
 public class Wallet
 {
-    public WalletWords WalletWords { get; set; } // todo: this will go away when we add passwords
     public FounderKeyCollection FounderKeys { get; set; }
-    public WalletPayload Payload { get; set; }
+    public string EncryptedData { get; set; }
 }
