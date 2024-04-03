57227b0c687ef83edef02311bdf880675a299dee
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2024-04-03 22:51:46 +0400

    Fixed wallet UI (#63)
    * Remove extra js codes
    
    * Fix wallet UI except modals
    
    * Fixed create and recover modal UI
    
    * Fixed wallet words modad UI
    
    * Fixed tables UI
    
    * Fixed checkbox UI

--- src/Angor/Client/wwwroot/assets/js/app.js
+++ src/Angor/Client/wwwroot/assets/js/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 window.angor = {
     installApp: function () {
@@ -6,21 +6,65 @@ window.angor = {
     },
 
     setTheme: function () {
-        const body = document.getElementsByTagName('body')[0];
+        const body = document.body;
         const themeIcon = document.getElementById('theme-icon');
         const theme = localStorage.getItem('theme');
 
         if (theme === 'dark') {
             body.classList.add('dark-version');
-            themeIcon.textContent = 'light_mode'; 
-            localStorage.setItem('theme', 'dark');
-        }  
+            themeIcon.textContent = 'light_mode';
+        } else {
+            body.classList.remove('dark-version');
+            themeIcon.textContent = 'dark_mode';
+        }
     },
 
     showSnackbar: function (message, delaySeconds) {
-        var x = document.getElementById("snackbar");
-        x.innerHTML = message;
-        x.className = "show";
-        setTimeout(function () { x.className = x.className.replace("show", ""); }, (delaySeconds * 1000));
+        const snackbar = document.getElementById('snackbar');
+        snackbar.innerHTML = message;
+        snackbar.classList.add('show');
+        setTimeout(function () { snackbar.classList.remove('show'); }, (delaySeconds * 1000));
     }
 };
+
+ 
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('resize', handleSidenavTypeOnResize);
+    window.addEventListener('load', handleSidenavTypeOnResize);
+});
+
+function handleSidenavTypeOnResize() {
+    const elements = document.querySelectorAll('[onclick="handleSidebarType(this)"]');
+    elements.forEach(element => {
+        const isDisabled = window.innerWidth < 1200;
+        element.classList.toggle('disabled', isDisabled);
+    });
+}
+
+
+
+
+
+function toggleSidenav() {
+    document.body.classList.toggle('sidenav-pinned');
+}
+
+function toggleDarkMode() {
+    const body = document.body;
+    const themeIcon = document.getElementById('theme-icon');
+    const currentTheme = body.classList.contains('dark-version') ? 'light' : 'dark';
+
+    if (currentTheme === 'light') {
+        body.classList.remove('dark-version');
+        themeIcon.textContent = 'dark_mode';
+    } else {
+        body.classList.add('dark-version');
+        themeIcon.textContent = 'light_mode';
+    }
+
+    localStorage.setItem('theme', currentTheme);
+}
+
