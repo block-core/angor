001ed7a67ce47bba6d6e738d32e78a3bc2f973aa
ilior <78041027+OgFenix@users.noreply.github.com>
2024-04-05 13:59:56 +0300

    Adding cypress to angor (#65)
    * add cypress install
    
    * update test
    
    there were some UI changes
    
    * Added cypress ids for UI elements
    
    Added cypress ids for UI elements and updated test accordingly

---
+++ cypress/e2e/wallet.cy.js
@@ -0,0 +1,21 @@
+import '../support/commands'
+import {Navbar,WALLET_DATA_CY} from '../support/enums'
+
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visitLocalhost();
+  });
+
+  it('createWallet', () => {
+    cy.clickOnNavBar(Navbar.WALLET)
+    cy.clickElementWithDataCy(WALLET_DATA_CY.CREATE_WALLET)
+    cy.clickElementWithDataCy(WALLET_DATA_CY.GENERATE_WALLET_WORDS)
+    cy.clickSubmitButton('New wallet password is null or empty');
+    cy.typeTextInElement('password','abc123')
+    cy.clickOnCheckBoxByDataCy(WALLET_DATA_CY.WALLET_CHECKBOX);
+    cy.clickSubmitButton();
+    cy.waitForLoader()
+    cy.get(`[data-cy=${WALLET_DATA_CY.BALANCE}]`).should('have.text', 'Balance: ');
+    cy.verifyBalance('0',WALLET_DATA_CY.BALANCE_AMOUNT)
+  });
+});
