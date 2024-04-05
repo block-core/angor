41fb3bd3f927539a9dab2901fcf7cf0f6a1f367a
ilior <78041027+OgFenix@users.noreply.github.com>
2024-04-05 13:59:56 +0300

    Adding cypress to angor (#65)
    * add cypress install
    
    * update test
    
    there were some UI changes
    
    * Added cypress ids for UI elements
    
    Added cypress ids for UI elements and updated test accordingly

---
+++ cypress/support/commands.js
@@ -0,0 +1,71 @@
+
+Cypress.Commands.add('visitLocalhost', () => {
+    // Set the viewport to a desktop resolution
+    cy.viewport(1280, 720); // You can adjust the width and height as needed
+    cy.visit('http://localhost:5062/');
+    //  cy.get('.loader').should('not.exist'); // Wait for loader to disappear
+    cy.get('span[role="button"].material-icons.opacity-10.btn-angor.fs-3#theme-icon').should('be.visible').click(); // Interact with the theme icon
+}); 
+
+Cypress.Commands.add('clickOnNavBar', (dir) => {
+    cy.get(`[href="${dir}"]`).should('be.visible').click();
+});
+
+Cypress.Commands.add('clickOnButtonContain', (name) => {
+    cy.log(name)
+    cy.get('.btn.btn-primary')
+        .contains(`${name}`)
+        .filter(':visible') // Filter to only visible elements
+        .click(); // Click on it
+})
+
+Cypress.Commands.add('clickElementWithDataCy', (datacy) => {
+    cy.get(`[data-cy=${datacy}]`).click();
+})
+
+
+Cypress.Commands.add("clickCardContains", (name, msg) => {
+    cy.get('.card-body[role="button"]').contains(`${name}`).click();
+    cy.log(msg)
+    if(msg){
+        cy.get('.snackbar').should('be.visible'); // Assuming snackbar has a class 'snackbar'
+        cy.get('.snackbar').contains(msg); // Assuming snackbar contains text
+    }
+  })
+  
+
+
+
+Cypress.Commands.add('clickSubmitButton', (msg) => {
+    cy.get('.btn.btn-success').click();
+    if(msg){
+        cy.get('#snackbar').should('be.visible'); // Assuming snackbar has a class 'snackbar'
+        cy.get('#snackbar').contains(msg); // Assuming snackbar contains text
+    }
+})
+
+Cypress.Commands.add('typeTextInElement', (type,msg) => {
+    cy.get(`.input-group input[type=${type}]`)
+        .type(msg);
+})
+
+Cypress.Commands.add('clickOnCheckBox', () => {
+    cy.get(`.form-check input[type="checkbox"]`)
+  .check();
+})
+
+Cypress.Commands.add('clickOnCheckBoxByDataCy', (datacy) => {
+    cy.get(`[data-cy=${datacy}]`).check();
+})
+
+Cypress.Commands.add('waitForLoader', () => {
+    cy.get('.loader').should('not.exist');
+})
+
+Cypress.Commands.add('verifyBalance', (num,datacy) => {
+    cy.get(`[data-cy=${datacy}]`)
+        .should('be.visible')
+        .contains(`${num}`);
+})
+
+
---
+++ cypress/support/enums.js
@@ -0,0 +1,15 @@
+const Navbar = {
+    WALLET: 'wallet',
+    CREATE_WALLET: 'Create Wallet',
+    GENERATE_WALLET: 'Generate New Wallet Words',
+};
+
+const WALLET_DATA_CY = {
+    CREATE_WALLET: 'create-wallet',
+    GENERATE_WALLET_WORDS: 'generate-wallet-words',
+    BALANCE: 'balance',
+    BALANCE_AMOUNT: 'balance-amount',
+    WALLET_CHECKBOX: 'wallet-checkbox',
+};
+
+export {Navbar,WALLET_DATA_CY};
