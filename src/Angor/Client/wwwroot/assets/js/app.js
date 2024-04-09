db5d1f3671f30870d8694cde619e56ca80112d21
Milad Raeisi <miladsoft@yahoo.com>
2024-04-09 10:16:50 +0400

    Create bundleconfig and convert js codes to blazor

--- src/Angor/Client/wwwroot/assets/js/app.js
+++ src/Angor/Client/wwwroot/assets/js/app.js
@@ -5,20 +5,6 @@ window.angor = {
         window.AngorAPP.installPWA();
     },
 
-    setTheme: function () {
-        const body = document.body;
-        const themeIcon = document.getElementById('theme-icon');
-        const theme = localStorage.getItem('theme');
-
-        if (theme === 'dark') {
-            body.classList.add('dark-version');
-            themeIcon.textContent = 'light_mode';
-        } else {
-            body.classList.remove('dark-version');
-            themeIcon.textContent = 'dark_mode';
-        }
-    },
-
     showSnackbar: function (message, delaySeconds) {
         const snackbar = document.getElementById('snackbar');
         snackbar.innerHTML = message;
@@ -26,45 +12,3 @@ window.angor = {
         setTimeout(function () { snackbar.classList.remove('show'); }, (delaySeconds * 1000));
     }
 };
-
- 
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('resize', handleSidenavTypeOnResize);
-    window.addEventListener('load', handleSidenavTypeOnResize);
-});
-
-function handleSidenavTypeOnResize() {
-    const elements = document.querySelectorAll('[onclick="handleSidebarType(this)"]');
-    elements.forEach(element => {
-        const isDisabled = window.innerWidth < 1200;
-        element.classList.toggle('disabled', isDisabled);
-    });
-}
-
-
-
-
-
-function toggleSidenav() {
-    document.body.classList.toggle('sidenav-pinned');
-}
-
-function toggleDarkMode() {
-    const body = document.body;
-    const themeIcon = document.getElementById('theme-icon');
-    const currentTheme = body.classList.contains('dark-version') ? 'light' : 'dark';
-
-    if (currentTheme === 'light') {
-        body.classList.remove('dark-version');
-        themeIcon.textContent = 'dark_mode';
-    } else {
-        body.classList.add('dark-version');
-        themeIcon.textContent = 'light_mode';
-    }
-
-    localStorage.setItem('theme', currentTheme);
-}
-
