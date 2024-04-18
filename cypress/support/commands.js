986401747d5e803ea36ec75dfde770cb4d34e01c
ilior <78041027+itailiors@users.noreply.github.com>
2024-04-18 13:13:19 +0300

    change wait for 30

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,7 +3,7 @@ Cypress.Commands.add('visitLocalhost', () => {
     // Set the viewport to a desktop resolution
     cy.viewport(1280, 720); // You can adjust the width and height as needed
     cy.visit('http://localhost:5062/');
-    cy.wait(10000);
+    cy.wait(30000);
     cy.get('.loader-wrapper').should('not.exist'); // Wait for loader to disappear
     cy.get('span[role="button"].material-icons.opacity-10.btn-angor.fs-3#theme-icon').should('be.visible').click(); // Interact with the theme icon
 }); 
