b17f23dc8eaed07cb5adadf98b620eb0dcb78055
itailiors <itailior171@gmail.com>
2024-04-21 13:32:37 +0300

    change dir name

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   e2e: {
-    specPattern: "src/testing/e2e/*.js",
+    specPattern: "src/Testing/e2e/*.js",
     supportFile: false, // Disable support file
     setupNodeEvents(on, config) {
       // implement node event listeners here
