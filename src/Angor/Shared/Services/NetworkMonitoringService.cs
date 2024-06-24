9c448459c841db76475e357878ede06c4f245858
Milad Raeisi <miladsoft@yahoo.com>
2024-06-25 00:47:20 +0330

    Create NetworkMonitoringService.cs

---
+++ src/Angor/Shared/Services/NetworkMonitoringService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+using Angor.Shared.Services;
+
+public class NetworkMonitoringService : BackgroundService
+{
+    private readonly INetworkService _networkService;
+    private readonly TimeSpan _checkInterval = TimeSpan.FromMinutes(1);
+
+    public NetworkMonitoringService(INetworkService networkService)
+    {
+        _networkService = networkService;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await _networkService.CheckServices(true);
+
+            await Task.Delay(_checkInterval, stoppingToken);
+        }
+    }
+}
