477a222efff68937d871d281fb7d434c1cd2d29b
dangershony <dan.gershony@gmail.com>
2024-11-04 14:18:12 +0000

    Prepare to add more networks

--- src/Angor/Shared/Networks/Networks.cs
+++ src/Angor/Shared/Networks/Networks.cs
@@ -4,14 +4,38 @@ namespace Angor.Shared.Networks
 {
     public static class Networks
     {
-        public static NetworksSelector Bitcoin
+        public static AngorNetworksSelector Bitcoin
         {
             get
             {
-                return new NetworksSelector(() => new BitcoinMain(), () => new BitcoinTest(), () => null);
+                return new AngorNetworksSelector(() => new BitcoinMain(), () => new BitcoinTest(), () => null);
             }
         }
 
      
+    }
+
+    public class AngorNetworksSelector
+    {
+        public AngorNetworksSelector(Func<Network> mainnet, Func<Network> testnet, Func<Network> regtest)
+        {
+            this.Mainnet = mainnet;
+            this.Testnet = testnet;
+            this.Regtest = regtest;
+        }
+
+        public Func<Network> Mainnet { get; }
+
+        public Func<Network> Testnet { get; }
+
+        public Func<Network> Testnet4 { get; }
+
+        public Func<Network> Regtest { get; }
+
+        public Func<Network> AngorNet { get; }
+
+        public Func<Network> SigNet { get; }
+
+        public Func<Network> MutinyNet { get; }
     }
 }
