e249ac1a35120fe98631828d13935d65c6a34661
itail <itailior171@gmail.com>
2024-11-19 07:37:18 +0200

    prototype- wip

---
+++ src/Angor/Client/wwwroot/service-worker-assets.js
@@ -0,0 +1,8 @@
+{
+    "assets": [
+    { "hash": "sha256-abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890", "url": "index.html" },
+    { "hash": "sha256-fedcba0987654321fedcba0987654321fedcba0987654321fedcba0987654321", "url": "manifest.json" },
+    { "hash": "sha256-123456abcdef123456abcdef123456abcdef123456abcdef123456abcdef123456", "url": "service-worker.js" },
+    { "hash": "sha256-abcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdef", "url": "assets/logo.png" }
+]
+}
--- src/Angor/Client/wwwroot/service-worker.js
+++ src/Angor/Client/wwwroot/service-worker.js
@@ -1,1 +1,34 @@
-self.addEventListener('fetch', () => { });
+importScripts('service-worker-assets.js');
+
+const cacheName = 'app-cache-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            const filesToCache = self.assets.map((asset) => asset.url);
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((key) => {
+                    if (key !== cacheName) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
