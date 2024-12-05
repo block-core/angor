81d341f7c137fd519478fc5989648c0632764b7b
SondreB <sondre@outlook.com>
2024-12-05 15:50:08 +0100

    Ensure less caching of Angor web app
    - Some of these settings can probably be removed after initial release, but during development they will help users be on latest versions.

--- src/Angor/Client/wwwroot/service-worker.js
+++ src/Angor/Client/wwwroot/service-worker.js
@@ -1,1 +1,23 @@
-self.addEventListener('fetch', () => { });
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            return caches.match(event.request);
+        })
+    );
+});
