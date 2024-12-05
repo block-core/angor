c062339a76535960a906f4fe0c977fa633d60b62
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2024-12-05 11:37:24 +0100

    Add Profile pictures

--- src/Angor/Avalonia/AngorApp/Sections/Browse/BrowseViewModel.cs
+++ src/Angor/Avalonia/AngorApp/Sections/Browse/BrowseViewModel.cs
@@ -11,15 +11,34 @@ public class BrowseViewModel : ReactiveObject, IBrowseViewModel
     {
         Projects =
         [
-            new Project("Ariton") { Picture = new Uri("https://ariton.app/assets/ariton-social.png") , ShortDescription = "Community Super App"},
-            new Project("Matrix 5") { Picture = new Uri("https://m.primal.net/KrhZ.jpg"), ShortDescription = "Matrix 5 Project" },
-            new Project("Bitcoin festival")  { Picture = new Uri("https://unchainedcrypto.com/wp-content/uploads/2023/10/bitcoin-hashrate.jpg") },
+            new Project("Space exploration")
+            {
+                Picture = new Uri("https://images.pexels.com/photos/998641/pexels-photo-998641.jpeg?auto=compress&cs=tinysrgb&w=600"),
+                Icon = new Uri("https://images.pexels.com/photos/998641/pexels-photo-998641.jpeg?auto=compress&cs=tinysrgb&w=600")
+            },
+            new Project("Ariton")
+            {
+                Picture = new Uri("https://ariton.app/assets/ariton-social.png"),
+                ShortDescription = "Community Super App",
+                Icon = new Uri("https://ariton.app/assets/community.webp")
+            },
+            new Project("Matrix 5")
+            {
+                Picture = new Uri("https://m.primal.net/KrhZ.jpg"),
+                ShortDescription = "Matrix 5 Project",
+                Icon = new Uri("https://pfp.nostr.build/5828e07a01a89d6059e85a00ca57680a1b835f2ad197afb2798ad8c7e175cf65.jpg")
+            },
+            new Project("Bitcoin festival")
+            {
+                Picture = new Uri("https://unchainedcrypto.com/wp-content/uploads/2023/10/bitcoin-hashrate.jpg"),
+                Icon = new Uri("https://unchainedcrypto.com/wp-content/uploads/2023/10/bitcoin-hashrate.jpg")
+            },
         ];
 
         OpenHub = ReactiveCommand.CreateFromTask(() => uiServices.LauncherService.Launch(new Uri("https://www.angor.io")));
     }
 
-    public ReactiveCommand<Unit,Unit> OpenHub { get; set; }
+    public ReactiveCommand<Unit, Unit> OpenHub { get; set; }
 
     public IReadOnlyCollection<Project> Projects { get; set; }
 }
--- src/Angor/Avalonia/AngorApp/Sections/Browse/Project.cs
+++ src/Angor/Avalonia/AngorApp/Sections/Browse/Project.cs
@@ -6,6 +6,7 @@ public class Project(string name)
 {
     public string Name { get; } = name;
     public object Picture { get; init; }
+    public object Icon { get; set; }
     public Uri? Uri { get; set; }
     public string ShortDescription { get; set; }
 }
