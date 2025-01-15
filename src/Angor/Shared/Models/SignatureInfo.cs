118c85a320c013029177e5501afc5aaa5adc0ec4
dangershony <dan.gershony@gmail.com>
2025-01-15 20:58:22 +0000

    Write the code to sign the release sigs

--- src/Angor/Shared/Models/SignatureInfo.cs
+++ src/Angor/Shared/Models/SignatureInfo.cs
@@ -6,4 +6,9 @@ public class SignatureInfo
     public List<SignatureInfoItem> Signatures { get; set; } = new();
     public DateTime? TimeOfSignatureRequest { get; set; }
     public string? SignatureRequestEventId { get; set; }
+
+    /// <summary>
+    /// Specify whether this a release or recovery signature type.
+    /// </summary>
+    public string SignatureType { get; set; }
 }
