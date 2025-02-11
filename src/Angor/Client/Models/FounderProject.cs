4cf8d6c4a39cd9377f4559d94bf65ae4cfecc279
dangershony <dan.gershony@gmail.com>
2025-02-11 13:49:11 +0000

    Removing the TargetInvestmentReached param

--- src/Angor/Client/Models/FounderProject.cs
+++ src/Angor/Client/Models/FounderProject.cs
@@ -10,24 +10,6 @@ public class FounderProject : Project
     public string ProjectInfoEventId { get; set; }
     public bool NostrProfileCreated { get; set; }
 
-
-    /// <summary>
-    /// The total amount of the project that has been invested in,
-    /// This parameter will only be set once the founder went to the spend page
-    /// and was able to calaulate to total amount of funds that have been invested in the project.
-    /// 
-    /// The intention is to use this parameter to know if the founder should be forced to release
-    /// the funds back to the investor by sending signature of a trx that spend coins to the investors address
-    /// </summary>
-    public decimal? TotalAvailableInvestedAmount { get; set; }
-
-    public DateTime? ReleaseSignaturesTime { get; set; }
-
-    public bool TargetInvestmentReached()
-    {
-        return TotalAvailableInvestedAmount >= ProjectInfo.TargetAmount;
-    }
-
     public bool ProjectHasStarted()
     {
         return DateTime.UtcNow > ProjectInfo.StartDate;
