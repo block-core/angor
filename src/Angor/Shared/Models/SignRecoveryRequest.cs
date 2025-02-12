8c6af84141850e52bdc401fe91960086ec9d2cdc
dangershony <dan.gershony@gmail.com>
2025-02-12 19:11:59 +0000

    Rename to UnfundedReleaseAddress

--- src/Angor/Shared/Models/SignRecoveryRequest.cs
+++ src/Angor/Shared/Models/SignRecoveryRequest.cs
@@ -4,17 +4,9 @@ public class SignRecoveryRequest
 {
     public string ProjectIdentifier { get; set; }
 
-    /// <summary>
-    /// An address that will be used to release the funds
-    /// to the investor in case the target amount is not reached.
-    /// </summary>
-    public string ReleaseAddress{ get; set; }
+    public string UnfundedReleaseAddress{ get; set; }
 
-    /// <summary>
-    /// A pubkey that will be converted by the founder to an address that will be used
-    /// to release the funds to the investor in case the target amount is not reached.
-    /// </summary>
-    public string ReleaseKey { get; set; }
+    public string UnfundedReleaseKey { get; set; }
 
     public string InvestmentTransactionHex { get; set; }
 }
