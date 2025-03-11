d58ff26ce9d368be03c544f4dd621a804bc9aa24
SuperJMN <superjmn@outlook.com>
2025-03-11 13:09:39 +0100

    Add TODO to PassphraseProviderAdapter

--- src/Angor/Avalonia/Angor.UI.Model.Implementation/Wallet/Password/PassphraseProviderAdapter.cs
+++ src/Angor/Avalonia/Angor.UI.Model.Implementation/Wallet/Password/PassphraseProviderAdapter.cs
@@ -9,6 +9,7 @@ public class  PassphraseProviderAdapter(IDialog dialog): IPassphraseProvider
 {
     public async Task<Maybe<string>> Get(WalletId walletId)
     {
+        // TODO: Implement the passphrase provider. It currently returns an empty string.
         return "";
         //return new DialogEncryptionKeyProvider(dialog, "Please, enter the passphrase").Get(walletId);
     }
