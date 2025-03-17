45c897cdc312371f2073f186483643f181277a81
david <DavidGershony@gmail.com>
2025-03-17 15:40:37 +0000

    Added implementation of nip 44

--- src/Angor/Client/Services/EncryptionService.cs
+++ src/Angor/Client/Services/EncryptionService.cs
@@ -1,8 +1,7 @@
-﻿using Blockcore.NBitcoin;
-using Blockcore.NBitcoin.Crypto;
+﻿using System.Security.Cryptography;
+using Blockcore.NBitcoin;
 using Blockcore.NBitcoin.DataEncoders;
 using Microsoft.JSInterop;
-using NBitcoin.Crypto;
 
 namespace Angor.Client.Services
 {
@@ -27,13 +26,15 @@ namespace Angor.Client.Services
 
         public async Task<string> EncryptNostrContentAsync(string nsec, string npub, string content)
         {
-            var secertHex = GetSharedSecretHexWithoutPrefix(nsec, npub);
+            //var secertHex = GetSharedSecretHexWithoutPrefix(nsec, npub);
+            var secertHex = GetAesConversationKey(nsec, npub);
             return await _jsRuntime.InvokeAsync<string>("encryptNostr", secertHex, content);
         }
 
         public async Task<string> DecryptNostrContentAsync(string nsec, string npub, string encryptedContent)
         {
-            var secertHex = GetSharedSecretHexWithoutPrefix(nsec, npub);
+            //var secertHex = GetSharedSecretHexWithoutPrefix(nsec, npub);
+            var secertHex = GetAesConversationKey(nsec, npub);
             return await _jsRuntime.InvokeAsync<string>("decryptNostr", secertHex, encryptedContent);
         }
 
@@ -44,6 +45,16 @@ namespace Angor.Client.Services
             
             var secert = publicKey.GetSharedPubkey(privateKey);
             return Encoders.Hex.EncodeData(secert.ToBytes()[1..]);
+        }
+        
+        private static byte[] GetAesConversationKey(string nsec, string npub)
+        {
+            var privateKey = new Key(Encoders.Hex.DecodeData(nsec));
+            var publicKey = new PubKey("02" + npub);
+            
+            var secert = publicKey.GetSharedPubkey(privateKey);
+
+            return HKDF.DeriveKey(HashAlgorithmName.SHA256, secert.ToBytes(), 32, "nip44-v2"u8.ToArray());
         }
     }
 }
--- src/Angor/Client/wwwroot/nostr-tools-methods.js
+++ src/Angor/Client/wwwroot/nostr-tools-methods.js
@@ -7,50 +7,60 @@ function hexToBytes(hex) {
     return new Uint8Array(bytes);
 }
 
+async function nip04Encryption(sharedSecretHex, message) {
+    const sharedSecret = hexToBytes(sharedSecretHex);
+
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message);
+
+    const iv = crypto.getRandomValues(new Uint8Array(16)); // Initialization vector for AES-CBC
+
+    const key = await crypto.subtle.importKey('raw', sharedSecret, 'AES-CBC', false, ['encrypt']);
+
+    const encrypted = await crypto.subtle.encrypt({
+        name: 'AES-CBC',
+        iv: iv
+    }, key, data);
+
+    const encryptedBase64 = Base64.fromUint8Array(new Uint8Array(encrypted));
+    const ivBase64 = Base64.fromUint8Array(iv);
+
+    return `${encryptedBase64}?iv=${ivBase64}`;
+}
+
+async function nip04Decryption(sharedSecretHex, encryptedMessage) {
+    const sharedSecret = hexToBytes(sharedSecretHex);
+
+    let [ctb64, ivb64] = encryptedMessage.split("?iv=")
+
+    const iv = Base64.toUint8Array(ivb64);
+    const ciphertext = Base64.toUint8Array(ctb64);
+
+    const key = await crypto.subtle.importKey('raw', sharedSecret, 'AES-CBC', false, ['decrypt']);
+
+    const decrypted = await crypto.subtle.decrypt({
+        name: 'AES-CBC',
+        iv: iv
+    }, key, ciphertext);
+
+    const decoder = new TextDecoder();
+    return decoder.decode(decrypted);
+}
+
 async function encryptNostr(sharedSecretHex, message) {
     try {
-        const sharedSecret = hexToBytes(sharedSecretHex);
-
-        const encoder = new TextEncoder();
-        const data = encoder.encode(message);
-
-        const iv = crypto.getRandomValues(new Uint8Array(16)); // Initialization vector for AES-CBC
-
-        const key = await crypto.subtle.importKey('raw', sharedSecret, 'AES-CBC', false, ['encrypt']);
-
-        const encrypted = await crypto.subtle.encrypt({
-            name: 'AES-CBC',
-            iv: iv
-        }, key, data);
-
-        const encryptedBase64 = Base64.fromUint8Array(new Uint8Array(encrypted));
-        const ivBase64 = Base64.fromUint8Array(iv);
-
-        return  `${encryptedBase64}?iv=${ivBase64}`;
+        return  NostrTools.nip44.encrypt(message,sharedSecretHex);
     } catch (e) {
         console.log(e)
         throw e;
     }
 }
 
+
+
 async function decryptNostr(sharedSecretHex, encryptedMessage) {
     try {
-        const sharedSecret = hexToBytes(sharedSecretHex);
-
-        let [ctb64, ivb64] = encryptedMessage.split("?iv=")
-
-        const iv = Base64.toUint8Array(ivb64);
-        const ciphertext = Base64.toUint8Array(ctb64);
-
-        const key = await crypto.subtle.importKey('raw', sharedSecret, 'AES-CBC', false, ['decrypt']);
-
-        const decrypted = await crypto.subtle.decrypt({
-            name: 'AES-CBC',
-            iv: iv
-        }, key, ciphertext);
-
-        const decoder = new TextDecoder();
-        return  decoder.decode(decrypted);
+        return NostrTools.nip44.v2.decrypt(encryptedMessage,sharedSecretHex);
     }
     catch (e)
     {
