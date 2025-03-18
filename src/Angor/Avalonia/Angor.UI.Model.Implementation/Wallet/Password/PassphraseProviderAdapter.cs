c749ca884addf4a6787d5f01ce6c7239519358ce
José Manuel Nieto <superjmn@outlook.com>
2025-03-18 10:54:27 +0100

    Implement passphrase retrieval in PassphraseProviderAdapter

--- src/Angor/Avalonia/Angor.UI.Model.Implementation/Wallet/Password/PassphraseProviderAdapter.cs
+++ src/Angor/Avalonia/Angor.UI.Model.Implementation/Wallet/Password/PassphraseProviderAdapter.cs
@@ -7,10 +7,8 @@ namespace Angor.UI.Model.Implementation.Wallet.Password;
 
 public class  PassphraseProviderAdapter(IDialog dialog): IPassphraseProvider
 {
-    public async Task<Maybe<string>> Get(WalletId walletId)
+    public Task<Maybe<string>> Get(WalletId walletId)
     {
-        // TODO: Implement the passphrase provider. It currently returns an empty string.
-        return "";
-        //return new DialogEncryptionKeyProvider(dialog, "Please, enter the passphrase").Get(walletId);
+        return new DialogEncryptionKeyProvider(dialog, "Please, enter the passphrase").Get(walletId);
     }
 }
