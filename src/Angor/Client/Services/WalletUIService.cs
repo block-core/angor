8b18b118ec8c2b4bc2ec1065d98a52be8f916509
dangershony <dan.gershony@gmail.com>
2025-03-19 17:49:39 +0000

    put the refresh code in its own service

--- src/Angor/Client/Services/WalletUIService.cs
+++ src/Angor/Client/Services/WalletUIService.cs
@@ -37,5 +37,32 @@ namespace Angor.Client.Services
             _cacheStorage.SetUnconfirmedInboundFunds(unconfirmedInbound);
             _cacheStorage.SetUnconfirmedOutboundFunds(unconfirmedOutbound);
         }
+
+        public async Task RefreshBalance(AccountBalanceInfo accountBalanceInfo)
+        {
+            try
+            {
+                var accountInfo = _storage.GetAccountInfo(_networkConfiguration.GetNetwork().Name);
+                var unconfirmedInboundFunds = _cacheStorage.GetUnconfirmedInboundFunds();
+
+                await _walletOperations.UpdateDataForExistingAddressesAsync(accountInfo);
+                await _walletOperations.UpdateAccountInfoWithNewAddressesAsync(accountInfo);
+
+                _storage.SetAccountInfo(_networkConfiguration.GetNetwork().Name, accountInfo);
+
+                var utxos = accountInfo.AllUtxos().Select(x => x.outpoint.ToString()).ToList();
+                var spentToUpdate = unconfirmedInboundFunds.RemoveAll(x => utxos.Contains(x.outpoint.ToString()));
+
+                if (spentToUpdate > 0)
+                    _cacheStorage.SetUnconfirmedInboundFunds(unconfirmedInboundFunds);
+
+                accountBalanceInfo.UpdateAccountBalanceInfo(accountInfo, unconfirmedInboundFunds);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error refreshing balance");
+                throw;
+            }
+        }
     }
 }
