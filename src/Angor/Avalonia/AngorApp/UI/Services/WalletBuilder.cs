cfed553314da43dbc109774d96e7b6cf8f0af209
SuperJMN <superjmn@outlook.com>
2025-04-09 09:36:47 +0200

    Correct sync mechanism

--- src/Angor/Avalonia/AngorApp/UI/Services/WalletBuilder.cs
+++ src/Angor/Avalonia/AngorApp/UI/Services/WalletBuilder.cs
@@ -7,10 +7,39 @@ namespace AngorApp.UI.Services;
 
 public class WalletBuilder(IWalletAppService walletAppService, ITransactionWatcher transactionWatcher) : IWalletBuilder
 {
+    private readonly Dictionary<WalletId, DynamicWallet> createdWallets = new ();
+    
     public async Task<Result<IWallet>> Create(WalletId walletId)
     {
+        if (createdWallets.ContainsKey(walletId))
+        {
+            throw new InvalidOperationException($"A DynamicWallet was created for the same WalletId: {walletId}. We should not create more than one instance.");
+        }
+        
         var dynamicWallet = new DynamicWallet(walletId, walletAppService, transactionWatcher);
-        var syncResult = await dynamicWallet.SyncCommand.StartReactive.Execute().FirstAsync();
-        return syncResult.Map(IWallet (_) => dynamicWallet);
+        
+        createdWallets.Add(walletId, dynamicWallet);
+        
+        var tcs = new TaskCompletionSource<Result<IWallet>>();
+
+        IDisposable syncSubscription = null!;
+        
+        dynamicWallet.SyncCommand.StartReactive.Take(1)
+            .Subscribe(result =>
+            {
+                if (result.IsSuccess)
+                {
+                    tcs.SetResult(Result.Success<IWallet>(dynamicWallet));
+                }
+                else
+                {
+                    tcs.SetResult(Result.Failure<IWallet>(result.Error));
+                    syncSubscription.Dispose();
+                }
+            });
+        
+        dynamicWallet.SyncCommand.StartReactive.Execute().Subscribe();
+        
+        return await tcs.Task;
     }
 }
