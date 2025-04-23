e7e043ec25d3c66223fe687b7af3a0df2fc7d8af
SuperJMN <superjmn@outlook.com>
2025-04-24 00:12:08 +0200

    Calculate Stages properly

--- src/Angor/Avalonia/AngorApp/Features/Invest/Amount/AmountViewModelDesign.cs
+++ src/Angor/Avalonia/AngorApp/Features/Invest/Amount/AmountViewModelDesign.cs
@@ -9,6 +9,14 @@ public class AmountViewModelDesign : IAmountViewModel
     public long? Amount { get; set; } = 20000;
     public decimal? AmountInBtc { get; set; }
     public IProject Project { get; } = new ProjectDesign();
+
+    public IEnumerable<StageBreakdown> StageBreakdowns { get; } = new List<StageBreakdown>
+    {
+        new StageBreakdown(1, 120, 0.2, DateTime.Now),
+        new StageBreakdown(1, 120, 0.2, DateTime.Now),
+        new StageBreakdown(1, 120, 0.2, DateTime.Now),
+    };
+    
     public IObservable<bool> IsValid => Observable.Return(true);
     public IObservable<bool> IsBusy => Observable.Return(false);
     public bool AutoAdvance => false;
--- src/Angor/Avalonia/AngorApp/Features/Invest/Amount/IAmountViewModel.cs
+++ src/Angor/Avalonia/AngorApp/Features/Invest/Amount/IAmountViewModel.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Zafiro.Avalonia.Controls.Wizards.Builder;
 
 namespace AngorApp.Features.Invest.Amount;
@@ -6,4 +7,17 @@ public interface IAmountViewModel : IStep
 {
     public long? Amount { get; set; }
     IProject Project { get; }
+    IEnumerable<StageBreakdown> StageBreakdowns { get; }
+}
+
+public record StageBreakdown(
+    int Index,
+    long Amount,
+    double Weight,
+    DateTime ReleaseDate)
+{
+    public long InvestmentSats => (long)(Amount * Weight);
+
+    public string Description =>
+        $"Stage {Index}: invest {InvestmentSats} sats that will be released on {ReleaseDate:d}";
 }
