0f186635342510bf65a01915cd6c1995ace73a69
SuperJMN <superjmn@outlook.com>
2025-04-29 12:49:44 +0200

    Switch IAmountUI to a separate file for better organization.
    Moved the IAmountUI interface out of AmountUI.cs and into its own file, IAmountUI.cs. This change improves code structure and keeps interfaces and classes separate for clarity and maintainability.

--- src/Angor/Avalonia/Angor.UI.Model/AmountUI.cs
+++ src/Angor/Avalonia/Angor.UI.Model/AmountUI.cs
@@ -2,25 +2,6 @@ using Angor.Contexts.Wallet.Domain;
 
 namespace Angor.UI.Model;
 
-public interface IAmountUI
-{
-    long Sats { get; }
-    public string BtcString
-    {
-        get
-        {
-            var btc = Sats / (decimal)10000_0000;
-            return $"{btc:0.0000 0000}" + " BTC";
-        }
-    }
-
-    public string SatsString => $"{Sats} sats";
-    public string FeeRateString => $"{Sats} sats/VByte";
-    public bool IsNegative => Sats < 0;
-    public bool IsPositive => Sats > 0;
-    public bool IsZero => Sats == 0;
-}
-
 public class AmountUI(long sats) : IAmountUI
 {
     public long Sats { get; } = sats;
