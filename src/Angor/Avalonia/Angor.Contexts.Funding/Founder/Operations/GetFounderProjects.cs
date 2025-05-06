ffbafdd163251ca61a8f7a30b50ac868c04412fc
SuperJMN <superjmn@outlook.com>
2025-05-06 17:18:05 +0200

    Improve readability

---
+++ src/Angor/Avalonia/Angor.Contexts.Funding/Founder/Operations/GetFounderProjects.cs
@@ -0,0 +1,41 @@
+using Angor.Contests.CrossCutting;
+using Angor.Contexts.Funding.Projects.Application.Dtos;
+using Angor.Contexts.Funding.Projects.Domain;
+using Angor.Contexts.Funding.Projects.Infrastructure.Impl;
+using Angor.Contexts.Funding.Shared;
+using Angor.Shared;
+using Angor.Shared.Models;
+using CSharpFunctionalExtensions;
+using MediatR;
+using Zafiro.CSharpFunctionalExtensions;
+
+namespace Angor.Contexts.Funding.Founder.Operations;
+
+public static class GetFounderProjects
+{
+    public class GetFounderProjectsHandler(
+        IProjectRepository projectRepository,
+        ISeedwordsProvider seedwordsProvider,
+        IDerivationOperations derivationOperations, 
+        INetworkConfiguration networkConfiguration) : IRequestHandler<GetFounderProjectsRequest, Result<IEnumerable<ProjectDto>>>
+    {
+        public Task<Result<IEnumerable<ProjectDto>>> Handle(GetFounderProjectsRequest request, CancellationToken cancellationToken)
+        {
+            return GetProjectIds(request)
+                .Traverse(projectRepository.Get)
+                .MapEach(project => project.ToDto());
+        }
+
+        private Task<Result<IEnumerable<ProjectId>>> GetProjectIds(GetFounderProjectsRequest request)
+        {
+            return seedwordsProvider.GetSensitiveData(request.WalletId)
+                .Map(p => p.ToWalletWords())
+                .Map(words => derivationOperations.DeriveProjectKeys(words, networkConfiguration.GetAngorKey()))
+                .Map(collection => collection.Keys.AsEnumerable())
+                .MapEach(keys => keys.ProjectIdentifier)
+                .MapEach(fk => new ProjectId(fk));
+        }
+    }
+
+    public record GetFounderProjectsRequest(Guid WalletId) : IRequest<Result<IEnumerable<ProjectDto>>>;
+}
