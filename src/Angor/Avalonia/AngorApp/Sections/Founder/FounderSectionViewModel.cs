6d4b89d3751b2fcc183299f56b72aaed8c10b637
SuperJMN <superjmn@outlook.com>
2025-05-07 19:02:17 +0200

    Improve command design

--- src/Angor/Avalonia/AngorApp/Sections/Founder/FounderSectionViewModel.cs
+++ src/Angor/Avalonia/AngorApp/Sections/Founder/FounderSectionViewModel.cs
@@ -1,27 +1,28 @@
-using Angor.Contexts.Funding.Founder.Operations;
 using Angor.Contexts.Funding.Projects.Application.Dtos;
 using Angor.Contexts.Funding.Projects.Infrastructure.Interfaces;
 using AngorApp.UI.Services;
 using DynamicData;
 using Zafiro.CSharpFunctionalExtensions;
 using Zafiro.UI;
+using Zafiro.UI.Commands;
 
 namespace AngorApp.Sections.Founder;
 
-public partial class FounderSectionViewModel : ReactiveObject, IFounderSectionViewModel
+public class FounderSectionViewModel : ReactiveObject, IFounderSectionViewModel
 {
     public FounderSectionViewModel(UIServices uiServices, IProjectAppService projectAppService, Func<ProjectDto, IFounderProjectViewModel> projectViewModelFactory)
     {
-        LoadProjects = ReactiveCommand.CreateFromTask(() =>
+        LoadProjects = EnhancedCommand.Create(ReactiveCommand.CreateFromTask(() =>
         {
             return uiServices.WalletRoot.GetDefaultWalletAndActivate()
                 .Bind(maybeWallet => maybeWallet
                     .ToResult("Please, create a wallet first")
                     .Bind(wallet => projectAppService.GetFounderProjects(wallet.Id.Value)));
-        });
+        }));
         
         LoadProjects.HandleErrorsWith(uiServices.NotificationService, "Failed to get pending investments");
-        LoadProjects.Successes().EditDiff(dto => dto.Id)
+        LoadProjects.Successes()
+            .EditDiff(dto => dto.Id)
             .Transform(projectViewModelFactory)
             .Bind(out var projectList)
             .Subscribe();
@@ -31,5 +32,5 @@ public partial class FounderSectionViewModel : ReactiveObject, IFounderSectionVi
 
     public IEnumerable<IFounderProjectViewModel> Projects { get; }
     
-    public ReactiveCommand<Unit, Result<IEnumerable<ProjectDto>>> LoadProjects { get; }
+    public IEnhancedCommand<Unit, Result<IEnumerable<ProjectDto>>> LoadProjects { get; }
 }
