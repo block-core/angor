769dbedc2f771e0ead4459ebee2771df291c64f4
SuperJMN <superjmn@outlook.com>
2025-06-10 19:05:29 +0200

    Simplify `ApproveInvestment` method and remove unused imports in `IInvestmentAppService`
    - Replaced `GetInvestments.Investment` parameter type with `Investment` in `ApproveInvestment` for consistency.
    - Removed unnecessary `Founder.Operations` and `Dtos` imports to clean up the interface file.

--- src/Angor/Avalonia/Angor.Contexts.Funding/Investor/IInvestmentAppService.cs
+++ src/Angor/Avalonia/Angor.Contexts.Funding/Investor/IInvestmentAppService.cs
@@ -1,5 +1,3 @@
-using Angor.Contexts.Funding.Founder.Operations;
-using Angor.Contexts.Funding.Investor.Dtos;
 using Angor.Contexts.Funding.Investor.Operations;
 using Angor.Contexts.Funding.Projects.Domain;
 using CSharpFunctionalExtensions;
@@ -12,7 +10,7 @@ public interface IInvestmentAppService
     Task<Result<CreateInvestment.Draft>> CreateInvestmentDraft(Guid sourceWalletId, ProjectId projectId, Amount amount);
     Task<Result<Guid>> Invest(Guid sourceWalletId, ProjectId projectId, CreateInvestment.Draft draft);
     Task<Result<IEnumerable<Investment>>> GetInvestments(Guid walletId, ProjectId projectId);
-    Task<Result> ApproveInvestment(Guid walletId, ProjectId projectId, GetInvestments.Investment investment);
+    Task<Result> ApproveInvestment(Guid walletId, ProjectId projectId, Investment investment);
     Task<Result<IEnumerable<InvestedProjectDto>>> GetInvestorProjects(Guid idValue);
     Task<Result> ConfirmInvestment(int investmentId);
 }
