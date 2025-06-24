31e2dd804112eeca0bf26889fbda16aea500d5d6
SuperJMN <superjmn@outlook.com>
2025-06-24 11:51:33 +0200

    Adjust investment wizard wording for better clarity:
    - Update button text from "Invest" to "Offer Investment" to align with process intent.
    - Revise confirmation message to clarify the investment offer review process.

--- src/Angor/Avalonia/AngorApp/Features/Invest/InvestWizard.cs
+++ src/Angor/Avalonia/AngorApp/Features/Invest/InvestWizard.cs
@@ -15,8 +15,8 @@ public class InvestWizard(IInvestmentAppService investmentAppService, UIServices
     public Task<Maybe<Unit>> Invest(IWallet wallet, IProject project)
     {
         var wizard = WizardBuilder.StartWith(() => new AmountViewModel(wallet, project), model => ReactiveCommand.Create(() => Result.Success(model.Amount), model.IsValid).Enhance("Next"), $"Invest in {project.Name}")
-            .Then(amount => new DraftViewModel(investmentAppService, wallet, amount!.Value, project, uiServices), model => model.Confirm.Enhance("Invest"), "Amount")
-            .Then(_ => new SuccessViewModel($"Invested in {project.Name}"), _ => ReactiveCommand.Create(() => Result.Success(Unit.Default)).Enhance("Close"), "Investment Successful")
+            .Then(amount => new DraftViewModel(investmentAppService, wallet, amount!.Value, project, uiServices), model => model.Confirm.Enhance("Offer Investment"), "Amount")
+            .Then(_ => new SuccessViewModel($"The investment offer to {project.Name} has been sent. The project founder will review your investment offer soon and either accept it or reject it"), _ => ReactiveCommand.Create(() => Result.Success(Unit.Default)).Enhance("Close"), "Investment Successful")
             .WithCompletionFinalStep();
         
         return uiServices.Dialog.ShowWizard(wizard, @$"Invest in ""{project}""");
