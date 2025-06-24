202b4bf4283192b1143da5813f3cdf264584682e
SuperJMN <superjmn@outlook.com>
2025-06-24 10:18:20 +0200

    Improve stages breakdown

--- src/Angor/Avalonia/AngorApp/Features/Invest/Amount/AmountViewModelDesign.cs
+++ src/Angor/Avalonia/AngorApp/Features/Invest/Amount/AmountViewModelDesign.cs
@@ -1,23 +1,13 @@
-using System.Windows.Input;
-using AngorApp.Sections.Browse;
-
 namespace AngorApp.Features.Invest.Amount;
 
 public class AmountViewModelDesign : IAmountViewModel
 {
-    public ICommand Next { get; } = ReactiveCommand.Create(() => { });
     public long? Amount { get; set; } = 20000;
-    public decimal? AmountInBtc { get; set; }
-    public IProject Project { get; } = new ProjectDesign();
 
     public IEnumerable<Breakdown> StageBreakdowns { get; } = new List<Breakdown>
     {
-        new Breakdown(1, 120, 0.2, DateTime.Now),
-        new Breakdown(1, 120, 0.2, DateTime.Now),
-        new Breakdown(1, 120, 0.2, DateTime.Now),
+        new(1, new AmountUI(120), 0.2, DateTime.Now),
+        new(2, new AmountUI(120), 0.4, DateTime.Now.AddMonths(1)),
+        new(3, new AmountUI(120), 0.6, DateTime.Now.AddMonths(2).AddDays(5)),
     };
-    
-    public IObservable<bool> IsValid => Observable.Return(true);
-    public IObservable<bool> IsBusy => Observable.Return(false);
-    public bool AutoAdvance => false;
 }
--- src/Angor/Avalonia/AngorApp/Features/Invest/Amount/IAmountViewModel.cs
+++ src/Angor/Avalonia/AngorApp/Features/Invest/Amount/IAmountViewModel.cs
@@ -3,6 +3,5 @@ namespace AngorApp.Features.Invest.Amount;
 public interface IAmountViewModel
 {
     public long? Amount { get; set; }
-    IProject Project { get; }
     IEnumerable<Breakdown> StageBreakdowns { get; }
 }
