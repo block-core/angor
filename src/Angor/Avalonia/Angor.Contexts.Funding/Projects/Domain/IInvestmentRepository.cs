6e8b4e788f3587c7983f686ca4f301c1ca8dc330
david <DavidGershony@gmail.com>
2025-06-26 15:07:17 +0100

    Moved the control of published investments to the repository

--- src/Angor/Avalonia/Angor.Contexts.Funding/Projects/Domain/IInvestmentRepository.cs
+++ src/Angor/Avalonia/Angor.Contexts.Funding/Projects/Domain/IInvestmentRepository.cs
@@ -1,4 +1,5 @@
 using Angor.Contexts.Funding.Investor.Dtos;
+using Angor.Contexts.Funding.Investor.Operations;
 using CSharpFunctionalExtensions;
 
 namespace Angor.Contexts.Funding.Projects.Domain;
@@ -6,7 +7,6 @@ namespace Angor.Contexts.Funding.Projects.Domain;
 public interface IInvestmentRepository
 {
     Task<Result<IEnumerable<InvestmentDto>>> GetByProject(ProjectId projectId);
-    // Task<Result<PendingInvestment>> GetPendingInvestment(Guid walletId, ProjectId projectId);
-    // Task<Result<SignedInvestment>> GetSignedInvestment(Guid walletId, ProjectId projectId);
+    Task<Result<InvestmentRecords>> GetByWallet(Guid walletId);
     Task<Result> Add(Guid walletId, Investment investment);
 }
