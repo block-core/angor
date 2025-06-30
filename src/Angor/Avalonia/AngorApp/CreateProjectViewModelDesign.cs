2a0d4031e20fcd3c944caa11f52ca3a90cdc8a6d
SuperJMN <superjmn@outlook.com>
2025-07-01 00:51:09 +0200

    Enhance `CreateProjectViewModel` with new properties and commands, update `DetailsView` with improved layouts, bindings, and advanced parameters support.

--- src/Angor/Avalonia/AngorApp/CreateProjectViewModelDesign.cs
+++ src/Angor/Avalonia/AngorApp/CreateProjectViewModelDesign.cs
@@ -1,14 +1,20 @@
+using Zafiro.UI.Commands;
+
 namespace AngorApp;
 
 public class CreateProjectViewModelDesign : ICreateProjectViewModel
 {
     public IEnumerable<ICreateProjectStage> Stages { get; set; } = new List<ICreateProjectStage>();
     public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public IEnhancedCommand AddStage { get; }
+    public int? PenaltyDays { get; set; }
+    public DateTime? ExpiryDate { get; set; }
 }
 
 public class CreateProjectStageDesign : ICreateProjectStage
 {
     public double Percent { get; set; }
     public DateTimeOffset? StartDate { get; set; }
+    public IEnhancedCommand Remove { get; }
 }
