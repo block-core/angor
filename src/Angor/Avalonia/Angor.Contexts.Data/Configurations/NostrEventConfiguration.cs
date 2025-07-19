035f350f0f0a4d14098fb91c64215c0732fe632e
david <DavidGershony@gmail.com>
2025-07-19 11:08:15 +0100

    Added database and first entities

---
+++ src/Angor/Avalonia/Angor.Contexts.Data/Configurations/NostrEventConfiguration.cs
@@ -0,0 +1,55 @@
+// Configurations/NostrEventConfiguration.cs
+using Angor.Contexts.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Angor.Contexts.Data.Configurations;
+
+public class NostrEventConfiguration : IEntityTypeConfiguration<NostrEvent>
+{
+    public void Configure(EntityTypeBuilder<NostrEvent> builder)
+    {
+        builder.HasKey(e => e.Id);
+        
+        builder.Property(e => e.Id)
+            .HasMaxLength(64)
+            .IsRequired();
+            
+        builder.Property(e => e.PubKey)
+            .HasMaxLength(64)
+            .IsRequired();
+            
+        builder.Property(e => e.Kind)
+            .IsRequired();
+            
+        builder.Property(e => e.Content)
+            .HasMaxLength(4000)
+            .IsRequired();
+            
+        builder.Property(e => e.Signature)
+            .HasMaxLength(128)
+            .IsRequired();
+            
+        builder.Property(e => e.CreatedAt)
+            .IsRequired();
+            
+        // Tags conversion
+        builder.Property(e => e.Tags)
+            .HasConversion(
+                v => string.Join(',', v),
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList())
+            .HasMaxLength(2000);
+        
+        // Indexes
+        builder.HasIndex(e => e.PubKey);
+        builder.HasIndex(e => e.CreatedAt);
+        builder.HasIndex(e => e.Kind);
+        
+        // Foreign key relationship
+        builder.HasOne(e => e.User)
+            .WithMany(u => u.Events)
+            .HasForeignKey(e => e.PubKey)
+            .HasPrincipalKey(u => u.PubKey)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
