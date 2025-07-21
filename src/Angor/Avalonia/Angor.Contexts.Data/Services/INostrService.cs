9c16a036fcc97b06824af180977b84c557a6d9e0
david <DavidGershony@gmail.com>
2025-07-21 19:51:48 +0100

    Changed the responsibility of nostr client to a different class

--- src/Angor/Avalonia/Angor.Contexts.Data/Services/INostrService.cs
+++ src/Angor/Avalonia/Angor.Contexts.Data/Services/INostrService.cs
@@ -1,11 +1,12 @@
+using Nostr.Client.Messages;
 using Nostr.Client.Responses;
 
 namespace Angor.Contexts.Data.Services;
 
 public interface INostrService
 {
-    Task<List<NostrEventResponse>> GetEventsByKindAsync(int kind, params string[] eventIds);
-    Task<List<NostrEventResponse>> GetEventsByKindAsync(int kind, int timeoutSeconds = 10, params string[] eventIds);
+    Task<List<NostrEventResponse>> GetEventsByKindAsync(NostrKind kind, params string[] eventIds);
+    Task<List<NostrEventResponse>> GetEventsByKindAsync(NostrKind kind, int timeoutSeconds = 10, params string[] eventIds);
     Task ConnectToRelaysAsync();
     Task DisconnectFromRelaysAsync();
     void AddRelay(string relayUrl);
