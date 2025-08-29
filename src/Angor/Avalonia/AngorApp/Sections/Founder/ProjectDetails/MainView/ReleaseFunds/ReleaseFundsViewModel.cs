964a593b92e4ce32ba71adbbd65bc665abd173c1
SuperJMN <superjmn@outlook.com>
2025-08-29 13:23:54 +0200

    Refresh when something happened

--- src/Angor/Avalonia/AngorApp/Sections/Founder/ProjectDetails/MainView/ReleaseFunds/ReleaseFundsViewModel.cs
+++ src/Angor/Avalonia/AngorApp/Sections/Founder/ProjectDetails/MainView/ReleaseFunds/ReleaseFundsViewModel.cs
@@ -14,56 +14,80 @@ namespace AngorApp.Sections.Founder.ProjectDetails.MainView.ReleaseFunds;
 
 public partial class ReleaseFundsViewModel : ReactiveObject, IReleaseFundsViewModel, IDisposable
 {
-    [ObservableAsProperty]
-    private IEnumerable<IUnfundedProjectTransaction> transactions;
-
-    private readonly ProjectId projectId;
+    private readonly CompositeDisposable disposable = new();
 
     private readonly IInvestmentAppService investmentAppService;
 
+    private readonly ProjectId projectId;
+
     private readonly UIServices uiServices;
 
-    private readonly CompositeDisposable disposable = new();
+    [ObservableAsProperty] private IEnumerable<IUnfundedProjectTransaction> transactions;
 
     public ReleaseFundsViewModel(ProjectId projectId, IInvestmentAppService investmentAppService, UIServices uiServices)
     {
         this.projectId = projectId;
         this.investmentAppService = investmentAppService;
         this.uiServices = uiServices;
-        
-        LoadTransactions = WalletCommand.Create(wallet => investmentAppService.GetReleaseableTransactions(wallet.Id.Value, projectId)
-            .MapEach(dto => (IUnfundedProjectTransaction)new UnfundedProjectTransaction(wallet.Id.Value, projectId, dto, investmentAppService, uiServices)), uiServices.WalletRoot)
+
+        RefreshTransactions = WalletCommand.Create(wallet => investmentAppService.GetReleaseableTransactions(wallet.Id.Value, projectId)
+                .MapEach(IUnfundedProjectTransaction (dto) => new UnfundedProjectTransaction(wallet.Id.Value, projectId, dto, investmentAppService, uiServices))
+                .Map(enumerable => enumerable.ToList()), uiServices.WalletRoot)
             .DisposeWith(disposable);
 
-        transactionsHelper = LoadTransactions
+        transactionsHelper = RefreshTransactions
             .Successes()
             .ToProperty(this, model => model.Transactions)
             .DisposeWith(disposable);
 
-        LoadTransactions.Execute().Subscribe().DisposeWith(disposable);
         ReleaseAll = WalletCommand.Create(DoReleaseAll, uiServices.WalletRoot, this.WhenAnyValue(model => model.Transactions).NotNull())
             .Enhance()
             .DisposeWith(disposable);
+        
+        RefreshWhenAnyCommandExecutes().DisposeWith(disposable);
+        RefreshTransactions.Execute().Subscribe().DisposeWith(disposable);
+    }
+
+    public ReactiveCommand<Unit, Result<List<IUnfundedProjectTransaction>>> RefreshTransactions { get; }
+
+    public void Dispose()
+    {
+        disposable.Dispose();
+    }
+
+    public IEnhancedCommand<Maybe<Result>> ReleaseAll { get; }
+
+    private IDisposable RefreshWhenAnyCommandExecutes()
+    {
+        return new[]
+        {
+            OnReleaseAllExecuted(),
+            OnChildReleaseCommandExecuted()
+        }.Merge().InvokeCommand(RefreshTransactions);
+    }
+
+    private IObservable<Unit> OnReleaseAllExecuted()
+    {
+        return ReleaseAll.Values().ToSignal();
+    }
+
+    private IObservable<Unit> OnChildReleaseCommandExecuted()
+    {
+        return this.WhenAnyValue(model => model.Transactions)
+            .WhereNotNull()
+            .Select(enumerable => enumerable.Select(transaction => transaction.Release.Values()).Merge().ToSignal())
+            .Switch();
     }
 
     private Task<Maybe<Result>> DoReleaseAll(IWallet wallet)
     {
         var addresses = Transactions.Select(transaction => transaction.InvestorAddress);
-        
+
         return UserFlow.PromptAndNotify(
-            () => investmentAppService.ReleaseInvestorTransactions(wallet.Id.Value, projectId, addresses), uiServices, 
-            "Are you sure you want to release all the funds?", 
-            "Confirm Release All", 
+            () => investmentAppService.ReleaseInvestorTransactions(wallet.Id.Value, projectId, addresses), uiServices,
+            "Are you sure you want to release all the funds?",
+            "Confirm Release All",
             "Successfully released all",
             "Released All", e => $"Cannot release the funds {e}");
-    }
-
-    public ReactiveCommand<Unit, Result<IEnumerable<IUnfundedProjectTransaction>>> LoadTransactions { get; }
-
-    public IEnhancedCommand<Maybe<Result>> ReleaseAll { get; }
-
-    public void Dispose()
-    {
-        disposable.Dispose();
     }
 }
