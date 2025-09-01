bbac08e553598564ad1cb63a2e8f2492dacaaf67
SuperJMN <superjmn@outlook.com>
2025-09-01 10:59:10 +0200

    Show empty content in grids

--- src/Angor/Avalonia/AngorApp/Sections/Founder/ProjectDetails/MainView/ReleaseFunds/ReleaseFundsViewModelDesign.cs
+++ src/Angor/Avalonia/AngorApp/Sections/Founder/ProjectDetails/MainView/ReleaseFunds/ReleaseFundsViewModelDesign.cs
@@ -4,25 +4,27 @@ namespace AngorApp.Sections.Founder.ProjectDetails.MainView.ReleaseFunds;
 
 public class ReleaseFundsViewModelDesign : IReleaseFundsViewModel
 {
-    public IEnumerable<IUnfundedProjectTransaction> Transactions { get; } = new List<IUnfundedProjectTransaction>()
-    {
-        new UnfundedProjectTransactionDesign()
+    public IEnumerable<IUnfundedProjectTransaction> Transactions { get; } = (List<IUnfundedProjectTransaction>)
+    [
+        new UnfundedProjectTransactionDesign
         {
             Approved = DateTime.Now,
             Released = DateTime.Now,
             Arrived = DateTime.Now,
         },
-        new UnfundedProjectTransactionDesign()
+
+        new UnfundedProjectTransactionDesign
         {
             Approved = DateTime.Now,
             Arrived = DateTime.Now,
         },
-        new UnfundedProjectTransactionDesign()
+
+        new UnfundedProjectTransactionDesign
         {
             Approved = DateTime.Now,
             Arrived = DateTime.Now,
         }
-    };
+    ];
 
     public IEnhancedCommand<Maybe<Result>> ReleaseAll { get; }
 }
