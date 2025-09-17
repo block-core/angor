0d63ecc9d075571bf169117f56d151627f522d31
david <DavidGershony@gmail.com>
2025-09-17 15:34:51 +0100

    Implemented the method to get the funds for the investor page

--- src/Angor/Avalonia/Angor.Contexts.Funding/Investor/Operations/GetInvestorProjectRecovery.cs
+++ src/Angor/Avalonia/Angor.Contexts.Funding/Investor/Operations/GetInvestorProjectRecovery.cs
@@ -1,10 +1,12 @@
 using Angor.Contexts.Funding.Projects.Domain;
 using Angor.Contexts.Funding.Projects.Infrastructure.Impl;
 using Angor.Contexts.Funding.Investor.Dtos;
+using Angor.Contexts.Funding.Projects.Infrastructure.Interfaces;
 using Angor.Shared;
 using Angor.Shared.Models;
 using Angor.Shared.Protocol;
 using Angor.Shared.Services;
+using Blockcore.Consensus.TransactionInfo;
 using Blockcore.NBitcoin;
 using CSharpFunctionalExtensions;
 using MediatR;
@@ -16,84 +18,310 @@ public static class GetInvestorProjectRecovery
     public record GetInvestorProjectRecoveryRequest(Guid WalletId, ProjectId ProjectId) : IRequest<Result<InvestorProjectRecoveryDto>>;
 
     public class Handler(
-        IInvestmentRepository investmentRepository,
         IProjectRepository projectRepository,
+        IInvestmentRepository investmentRepository,
         IIndexerService indexerService,
         INetworkConfiguration networkConfiguration,
-        IInvestorTransactionActions investorTransactionActions
+        IInvestorTransactionActions investorTransactionActions,
+        IProjectInvestmentsRepository projectInvestmentsRepository
     ) : IRequestHandler<GetInvestorProjectRecoveryRequest, Result<InvestorProjectRecoveryDto>>
     {
-        public Task<Result<InvestorProjectRecoveryDto>> Handle(GetInvestorProjectRecoveryRequest request, CancellationToken cancellationToken)
+        public async Task<Result<InvestorProjectRecoveryDto>> Handle(GetInvestorProjectRecoveryRequest request,
+            CancellationToken cancellationToken)
         {
-            // TODO: Implement real logic here
-            var now = DateTime.UtcNow;
-            var expiry = now.AddDays(60);
+            var project = await projectRepository.Get(request.ProjectId);
 
-            var dto = new InvestorProjectRecoveryDto
+            if (project.IsFailure)
+                return Result.Failure<InvestorProjectRecoveryDto>(project.Error);
+            
+            var investments = await investmentRepository.GetByWalletId(request.WalletId);
+
+            if (investments.IsFailure)
+                return Result.Failure<InvestorProjectRecoveryDto>(investments.Error);
+
+            if (investments.Value.ProjectIdentifiers.Count == 0)
+                return Result.Failure<InvestorProjectRecoveryDto>("No investments found for this wallet");
+            
+            var investmentRecord = investments.Value.ProjectIdentifiers
+                .FirstOrDefault(x => x.ProjectIdentifier == request.ProjectId.Value);
+            
+            if (investmentRecord == null)
+                return Result.Failure<InvestorProjectRecoveryDto>("No investments found for this project");
+
+            var investmentDetails = await FindInvestments(project.Value, investmentRecord.InvestorPubKey);
+            
+            if (investmentDetails.IsFailure)
+                return Result.Failure<InvestorProjectRecoveryDto>(investmentDetails.Error);
+            
+            if (!investmentDetails.Value.Item2.Any())
+                return Result.Failure<InvestorProjectRecoveryDto>("No investment stages found for this project");
+
+            return await CheckSpentFund(investmentDetails.Value.Item2.ToList(), investmentDetails.Value.Item1, project.Value);
+            
+            // // TODO: Implement real logic here
+            // var now = DateTime.UtcNow;
+            // var expiry = now.AddDays(60);
+            //
+            // var dto = new InvestorProjectRecoveryDto
+            // {
+            //     ProjectIdentifier = request.ProjectId.Value,
+            //     Name = "Manage Investment (Test Data)",
+            //     ExpiryDate = expiry,
+            //     PenaltyDays = 90,
+            //     CanRecover = true,
+            //     CanRelease = true,
+            //     EndOfProject = false,
+            // };
+            //
+            // // 1) Not spent (eligible for Recover)
+            // dto.Items.Add(new InvestorStageItemDto
+            // {
+            //     StageIndex = 0,
+            //     Amount = 198_0000,
+            //     IsSpent = false,
+            //     Status = "Not Spent",
+            //     ScriptType = ProjectScriptTypeEnum.Unknown,
+            // });
+            //
+            // // 2) In penalty (not expired)
+            // dto.Items.Add(new InvestorStageItemDto
+            // {
+            //     StageIndex = 1,
+            //     Amount = 594_0000,
+            //     IsSpent = true,
+            //     Status = "Penalty, released in 37.1 days",
+            //     ScriptType = ProjectScriptTypeEnum.InvestorWithPenalty,
+            // });
+            //
+            // // 3) In penalty (expired)
+            // dto.Items.Add(new InvestorStageItemDto
+            // {
+            //     StageIndex = 2,
+            //     Amount = 1_188_0000,
+            //     IsSpent = true,
+            //     Status = "Penalty can be released",
+            //     ScriptType = ProjectScriptTypeEnum.InvestorWithPenalty,
+            // });
+            //
+            // // 4) Spent by founder
+            // dto.Items.Add(new InvestorStageItemDto
+            // {
+            //     StageIndex = 3,
+            //     Amount = 250_0000,
+            //     IsSpent = true,
+            //     Status = "Spent by founder",
+            //     ScriptType = ProjectScriptTypeEnum.Founder,
+            // });
+            //
+            // // 5) Spent by investor (unfunded release)
+            // dto.Items.Add(new InvestorStageItemDto
+            // {
+            //     StageIndex = 4,
+            //     Amount = 350_0000,
+            //     IsSpent = true,
+            //     Status = "Spent by investor",
+            //     ScriptType = ProjectScriptTypeEnum.InvestorNoPenalty,
+            // });
+            //
+            // dto.TotalSpendable = dto.Items.Where(i => !i.IsSpent).Sum(i => i.Amount);
+            // dto.TotalInPenalty = dto.Items.Where(i => i.ScriptType == ProjectScriptTypeEnum.InvestorWithPenalty)
+            //     .Sum(i => i.Amount);
+            //
+            // return Task.FromResult(Result.Success(dto));
+        }
+
+
+
+        private async Task<Result<(QueryTransaction,IEnumerable<InvestorStageItemDto>)>> FindInvestments(Project project,string investorPubKey)
+        {
+            var trx = await indexerService.GetInvestmentAsync(project.Id.Value, investorPubKey);
+            
+            if (trx == null || string.IsNullOrEmpty(trx.TransactionId) || trx.InvestorPublicKey != investorPubKey)
             {
-                ProjectIdentifier = request.ProjectId.Value,
-                Name = "Manage Investment (Test Data)",
-                ExpiryDate = expiry,
-                PenaltyDays = 90,
-                CanRecover = true,
-                CanRelease = true,
-                EndOfProject = false,
+                return Result.Failure<(QueryTransaction,IEnumerable<InvestorStageItemDto>)>("Investment transaction not found");
+            }
+
+            var trxInfo = await indexerService.GetTransactionInfoByIdAsync(trx.TransactionId);
+            if (trxInfo == null)
+            {
+                return Result.Failure<(QueryTransaction,IEnumerable<InvestorStageItemDto>)>("Investment transaction info not found");
+            }
+
+            var stagesData = Result.Try(() => project.Stages.Select((_, stageIndex) =>
+            {
+                var output = trxInfo.Outputs.First(f => f.Index == stageIndex + 2);
+                
+                return new InvestorStageItemDto
+                {
+                    StageIndex = stageIndex,
+                    Amount = output.Balance, // Store balance in satoshis
+                    IsSpent = false,
+                    Status = "Unknown",
+                    ScriptType = ProjectScriptTypeEnum.Unknown,
+                };
+            }));
+
+            return stagesData.IsFailure
+                ? Result.Failure<(QueryTransaction, IEnumerable<InvestorStageItemDto>)>(stagesData.Error)
+                : Result.Success<(QueryTransaction, IEnumerable<InvestorStageItemDto>)>((trxInfo, stagesData.Value));
+        }
+
+        private async Task<Result<InvestorProjectRecoveryDto>> CheckSpentFund(IList<InvestorStageItemDto> stageItems, QueryTransaction transactionInfo, Project project)
+        {
+            // var unconfirmedOutbound = _cacheStorage.GetUnconfirmedOutboundFunds();
+            // bool modified = false;
+            //
+            // foreach (var infoOutput in transactionInfo.Outputs)
+            // {
+            //     if (!string.IsNullOrEmpty(infoOutput.SpentInTransaction))
+            //     {
+            //         modified |= unconfirmedOutbound.TryRemoveOutpoint(new Outpoint(transactionInfo.TransactionId, infoOutput.Index));
+            //     }
+            // }
+            //
+            // if (modified)
+            // {
+            //     _cacheStorage.SetUnconfirmedOutboundFunds(unconfirmedOutbound);
+            // }
+
+            var projectInfo = project.ToProjectInfo();
+
+            var trxHex = await indexerService.GetTransactionHexByIdAsync(transactionInfo.TransactionId);
+            var investmentTransaction = networkConfiguration.GetNetwork().CreateTransaction(trxHex);
+
+            var lookup = await projectInvestmentsRepository.ScanInvestmentSpends(projectInfo, transactionInfo.TransactionId);
+
+            if (lookup.IsFailure)
+                return Result.Failure<InvestorProjectRecoveryDto>(lookup.Error);
+            
+            var penaltyExpieryDate =
+                Utils.UnixTimeToDateTime(transactionInfo.Timestamp).AddDays(projectInfo.PenaltyDays);
+
+            if (!string.IsNullOrEmpty(lookup.Value.RecoveryTransactionId))
+            {
+                var recoveryTansaction =
+                    await indexerService.GetTransactionInfoByIdAsync(lookup.Value.RecoveryTransactionId);
+                if (recoveryTansaction != null)
+                    penaltyExpieryDate = Utils.UnixTimeToDateTime(recoveryTansaction.Timestamp)
+                        .AddDays(projectInfo.PenaltyDays);
+            }
+
+
+            var tasks = stageItems.Select(item => CheckTransactionSpendingAsync(transactionInfo, item, lookup, penaltyExpieryDate,
+                investmentTransaction, projectInfo));
+            
+            await Task.WhenAll(tasks);
+
+            var response = new InvestorProjectRecoveryDto
+            {
+                CanRecover = stageItems.Any(a => a.IsSpent == false),
+                CanRelease = (stageItems.Any(a => a.ScriptType == ProjectScriptTypeEnum.InvestorWithPenalty) &&
+                              DateTime.UtcNow > penaltyExpieryDate),
+                TotalSpendable = stageItems.Where(a => !a.IsSpent).Sum(a => a.Amount),
+                TotalInPenalty = stageItems.Where(t => t.ScriptType == ProjectScriptTypeEnum.InvestorWithPenalty)
+                    .Sum(t => t.Amount),
+                EndOfProject = projectInfo.ExpiryDate < DateTime.Now,
+                ExpiryDate = projectInfo.ExpiryDate,
+                Name = project.Name,
+                PenaltyDays = projectInfo.PenaltyDays,
+                ProjectIdentifier = project.Id.Value,
+                Items = stageItems.ToList()
             };
 
-            // 1) Not spent (eligible for Recover)
-            dto.Items.Add(new InvestorStageItemDto
+            return Result.Success(response);
+        }
+
+        private async Task<Result> CheckTransactionSpendingAsync(QueryTransaction transactionInfo, InvestorStageItemDto item, Result<InvestmentSpendingLookup> lookup,
+            DateTimeOffset penaltyExpieryDate, Transaction? investmentTransaction, ProjectInfo projectInfo)
+        {
+            // if (unconfirmedOutbound.ContainsOutpoint(new Outpoint(StageInfo.TransactionInfo.TransactionId, item.Outputindex)))
+            // {
+            //     item.IsSpent = true;
+            //     item.SpentTo = "pending confirmations";
+            //     item.ProjectScriptType = new ProjectScriptType { ScriptType = ProjectScriptTypeEnum.Unknown };
+            //
+            //     continue;
+            // }
+            
+            var output = transactionInfo.Outputs.ElementAt(item.StageIndex + 2);
+
+            if (!string.IsNullOrEmpty(output.SpentInTransaction))
             {
-                StageIndex = 0,
-                Amount = 198_0000,
-                IsSpent = false,
-                Status = "Not Spent",
-                ScriptType = ProjectScriptTypeEnum.Unknown,
-            });
+                item.IsSpent = true;
 
-            // 2) In penalty (not expired)
-            dto.Items.Add(new InvestorStageItemDto
-            {
-                StageIndex = 1,
-                Amount = 594_0000,
-                IsSpent = true,
-                Status = "Penalty, released in 37.1 days",
-                ScriptType = ProjectScriptTypeEnum.InvestorWithPenalty,
-            });
+                if (output.SpentInTransaction == lookup.Value.UnfundedReleaseTransactionId)
+                {
+                    item.Status = "Project Unfunded, Spent back to investor";
+                    item.ScriptType = ProjectScriptTypeEnum.InvestorNoPenalty;
+                }
+                else
+                {
+                    if (output.SpentInTransaction == lookup.Value.RecoveryTransactionId)
+                    {
+                        if (!string.IsNullOrEmpty(lookup.Value.RecoveryReleaseTransactionId))
+                        {
+                            item.Status = "Recovered after penalty";
+                            item.ScriptType = ProjectScriptTypeEnum.Unknown;
+                        }
+                        else
+                        {
+                            item.ScriptType = ProjectScriptTypeEnum.InvestorWithPenalty;
+                            var days = (penaltyExpieryDate - DateTime.Now).TotalDays;
+                            item.Status = days > 0
+                                ? $"Penalty, released in {days.ToString("0.0")} days"
+                                : "Penalty can be released";
+                        }
+                    }
+                    else
+                    {
+                        // try to resolve the destination
+                        var spentInTransaction =
+                            await indexerService.GetTransactionInfoByIdAsync(output.SpentInTransaction);
 
-            // 3) In penalty (expired)
-            dto.Items.Add(new InvestorStageItemDto
-            {
-                StageIndex = 2,
-                Amount = 1_188_0000,
-                IsSpent = true,
-                Status = "Penalty can be released",
-                ScriptType = ProjectScriptTypeEnum.InvestorWithPenalty,
-            });
+                        var input = spentInTransaction?.Inputs.FirstOrDefault(input =>
+                            input.InputTransactionId == transactionInfo.TransactionId &&
+                            input.InputIndex == item.StageIndex + 2);
 
-            // 4) Spent by founder
-            dto.Items.Add(new InvestorStageItemDto
-            {
-                StageIndex = 3,
-                Amount = 250_0000,
-                IsSpent = true,
-                Status = "Spent by founder",
-                ScriptType = ProjectScriptTypeEnum.Founder,
-            });
+                        if (input != null && investmentTransaction != null)
+                        {
+                            var script = investorTransactionActions.DiscoverUsedScript(projectInfo,
+                                investmentTransaction, item.StageIndex, input.WitScript);
 
-            // 5) Spent by investor (unfunded release)
-            dto.Items.Add(new InvestorStageItemDto
-            {
-                StageIndex = 4,
-                Amount = 350_0000,
-                IsSpent = true,
-                Status = "Spent by investor",
-                ScriptType = ProjectScriptTypeEnum.InvestorNoPenalty,
-            });
+                            item.ScriptType = script.ScriptType;
 
-            dto.TotalSpendable = dto.Items.Where(i => !i.IsSpent).Sum(i => i.Amount);
-            dto.TotalInPenalty = dto.Items.Where(i => i.ScriptType == ProjectScriptTypeEnum.InvestorWithPenalty).Sum(i => i.Amount);
-
-            return Task.FromResult(Result.Success(dto));
+                            switch (script.ScriptType)
+                            {
+                                case ProjectScriptTypeEnum.Founder:
+                                {
+                                    item.Status = "Spent by founder";
+                                    break;
+                                }
+                                case ProjectScriptTypeEnum.InvestorWithPenalty:
+                                {
+                                    var days = (penaltyExpieryDate - DateTime.Now).TotalDays;
+                                    item.Status = days > 0
+                                        ? $"Penalty, released in {days.ToString("0.0")} days"
+                                        : "Penalty can be released";
+                                    break;
+                                }
+                                case ProjectScriptTypeEnum.EndOfProject:
+                                case ProjectScriptTypeEnum.InvestorNoPenalty:
+                                {
+                                    item.Status = $"Spent by investor";
+                                    break;
+                                }
+                                case ProjectScriptTypeEnum.Unknown:
+                                    break;
+                                default:
+                                    return Result.Failure(
+                                        $"Unknown script type {item.ScriptType} on stage {item.StageIndex}");
+                            }
+                        }
+                    }
+                }
+            }
+            
+            return Result.Success();
         }
     }
 }
