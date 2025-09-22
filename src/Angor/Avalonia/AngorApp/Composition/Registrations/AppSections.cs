8da6f95444f0a6c57714dabbe534a752213bc1c3
SuperJMN <superjmn@outlook.com>
2025-09-22 14:25:32 +0200

    Add dynamic sections for Wallet, Portfolio, Founder, and Settings

--- src/Angor/Avalonia/AngorApp/Composition/Registrations/AppSections.cs
+++ src/Angor/Avalonia/AngorApp/Composition/Registrations/AppSections.cs
@@ -1,13 +1,20 @@
 using AngorApp.Sections.Browse;
 using AngorApp.Sections.Home;
+using AngorApp.Sections.Founder;
+using AngorApp.Sections.Portfolio;
+using AngorApp.Sections.Settings;
+using AngorApp.Sections.Wallet;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Zafiro.UI;
 using Zafiro.UI.Navigation.Sections;
+using Zafiro.Avalonia.Services;
+using ReactiveUI;
+using System;
 
 namespace AngorApp.Composition.Registrations;
 
-public class AppSections
+public static class AppSections
 {
     public static void Register(ServiceCollection services, ILogger logger)
      {
@@ -27,14 +34,19 @@ public class AppSections
             return
             [
                 dynamicHome,
-                new ContentSection<IBrowseSectionViewModel>("Browse", Observable.Defer(() => Observable.Return(provider.GetRequiredService<IBrowseSectionViewModel>())), new Icon("svg:/Assets/browse.svg"))
+                new SectionSeparator(),
+                new ContentSection<IWalletSectionViewModel>("Wallet", Observable.Defer(() => Observable.Return(provider.GetRequiredService<IWalletSectionViewModel>())), new Icon("svg:/Assets/wallet.svg")),
+                new ContentSection<IBrowseSectionViewModel>("Browse", Observable.Defer(() => Observable.Return(provider.GetRequiredService<IBrowseSectionViewModel>())), new Icon("svg:/Assets/browse.svg")),
+                new ContentSection<IPortfolioSectionViewModel>("Portfolio", Observable.Defer(() => Observable.Return(provider.GetRequiredService<IPortfolioSectionViewModel>())), new Icon("svg:/Assets/portfolio.svg")),
+                new ContentSection<IFounderSectionViewModel>("Founder", Observable.Defer(() => Observable.Return(provider.GetRequiredService<IFounderSectionViewModel>())), new Icon("svg:/Assets/user.svg")),
+                new SectionSeparator(),
+                new ContentSection<ISettingsSectionViewModel>("Settings", Observable.Defer(() => Observable.Return(provider.GetRequiredService<ISettingsSectionViewModel>())), new Icon("svg:/Assets/settings.svg")),
+                new CommandSection("Angor Hub", ReactiveCommand.CreateFromTask(() => provider.GetRequiredService<ILauncherService>().LaunchUri(new Uri("https://hub.angor.io"))), new Icon("svg:/Assets/browse.svg")) { IsPrimary = false },
             ];
         });
 
         // services.RegisterSections(builder => builder.Add(new Section())
         //         .Add<IHomeSectionViewModel>("Home", new Icon { Source = "svg:/Assets/angor-icon.svg" })
-        //         .Add<Lightweight1>("Lightweight 1", new Icon { Source = "svg:/Assets/angor-icon.svg" })
-        //         .Add<Lightweight2>("Lightweight 2", new Icon { Source = "svg:/Assets/angor-icon.svg" })
         //         .Separator()
         //         .Add<IWalletSectionViewModel>("Wallet", new Icon { Source = "svg:/Assets/wallet.svg" })
         //         .Add<IBrowseSectionViewModel>("Browse", new Icon { Source = "svg:/Assets/browse.svg" })
