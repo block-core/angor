573856891e3ab7d451c83339375f4902b14d9a63
david <DavidGershony@gmail.com>
2025-10-15 20:06:40 +0100

    Added Result<> to the returned collection

--- src/Angor/Avalonia/Angor.Data.Documents/Interfaces/IDocumentCollection.cs
+++ src/Angor/Avalonia/Angor.Data.Documents/Interfaces/IDocumentCollection.cs
@@ -1,21 +1,23 @@
 using System.Linq.Expressions;
 using Angor.Data.Documents.Models;
+using CSharpFunctionalExtensions;
 
 namespace Angor.Data.Documents.Interfaces;
 
 public interface IDocumentCollection<T> where T : BaseDocument
 {
-    //read
-    Task<T?> FindByIdAsync(string id);
-    Task<IEnumerable<T>> FindAllAsync();
-    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
-    Task<bool> ExistsAsync(string id);
-    Task<IEnumerable<T>> GetAllAsync();
-    Task<int> CountAsync();
-    Task<long> CountAsync(Expression<Func<T, bool>>? predicate = null);
-    //write
-    Task<string> InsertAsync(T document);
-    Task<bool> UpdateAsync(T document);
-    Task<bool> UpsertAsync(T document);
-    Task<bool> DeleteAsync(string id);
+    // Read operations
+    Task<Result<T?>> FindByIdAsync(string id);
+    Task<Result<IEnumerable<T>>> FindAllAsync();
+    Task<Result<IEnumerable<T>>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<Result<bool>> ExistsAsync(string id);
+    Task<Result<IEnumerable<T>>> GetAllAsync();
+    Task<Result<int>> CountAsync();
+    Task<Result<long>> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    
+    // Write operations
+    Task<Result<string>> InsertAsync(T document);
+    Task<Result<bool>> UpdateAsync(T document);
+    Task<Result<bool>> UpsertAsync(T document);
+    Task<Result<bool>> DeleteAsync(string id);
 }
