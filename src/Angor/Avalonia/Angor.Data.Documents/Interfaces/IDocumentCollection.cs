23dbc1f9f8a6c1ea186720ab8a34331b6bebad25
david <DavidGershony@gmail.com>
2025-10-15 17:47:29 +0100

    Fixed compilation error

--- src/Angor/Avalonia/Angor.Data.Documents/Interfaces/IDocumentCollection.cs
+++ src/Angor/Avalonia/Angor.Data.Documents/Interfaces/IDocumentCollection.cs
@@ -5,13 +5,17 @@ namespace Angor.Data.Documents.Interfaces;
 
 public interface IDocumentCollection<T> where T : BaseDocument
 {
+    //read
     Task<T?> FindByIdAsync(string id);
     Task<IEnumerable<T>> FindAllAsync();
     Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<bool> ExistsAsync(string id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<int> CountAsync();
+    Task<long> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    //write
     Task<string> InsertAsync(T document);
     Task<bool> UpdateAsync(T document);
-    Task<bool> DeleteAsync(string id);
-    Task<int> CountAsync();
-    Task<bool> ExistsAsync(string id);
     Task<bool> UpsertAsync(T document);
+    Task<bool> DeleteAsync(string id);
 }
