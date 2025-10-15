73494412bbd7b9dd2c1aadaa5fbc5a97f8511d68
david <DavidGershony@gmail.com>
2025-10-15 17:47:29 +0100

    Fixed compilation error

--- src/Angor/Avalonia/Angor.Data.Documents/Interfaces/IAngorDocumentDatabase.cs
+++ src/Angor/Avalonia/Angor.Data.Documents/Interfaces/IAngorDocumentDatabase.cs
@@ -2,12 +2,16 @@ using Angor.Data.Documents.Models;
 
 namespace Angor.Data.Documents.Interfaces;
 
-public interface IAngorDocumentDatabase : IDisposable
+public interface IAngorDocumentDatabase
 {
+    IDocumentCollection<T> GetCollection<T>() where T : BaseDocument;
     IDocumentCollection<T> GetCollection<T>(string? name = null) where T : BaseDocument;
+    
     Task<bool> BeginTransactionAsync();
     Task<bool> CommitAsync();
+    Task<bool> CommitTransactionAsync();
     Task<bool> RollbackAsync();
+    Task<bool> RollbackTransactionAsync();
     Task<bool> DatabaseExistsAsync();
     Task<long> GetDatabaseSizeAsync();
 }
