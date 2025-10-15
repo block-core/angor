9b8e730f9f614aaee38e4945c93207785c884433
david <DavidGershony@gmail.com>
2025-10-15 16:53:29 +0100

    Added the database projects split for abstraction and selected db and added first interfaces

--- src/Angor/Avalonia/Angor.Data.Documents/Interfaces/IAngorDocumentDatabase.cs
+++ src/Angor/Avalonia/Angor.Data.Documents/Interfaces/IAngorDocumentDatabase.cs
@@ -3,6 +3,7 @@ using Angor.Data.Documents.Models;
 namespace Angor.Data.Documents.Interfaces;
 
 public interface IAngorDocumentDatabase
+public interface IAngorDocumentDatabase : IDisposable
 {
     IDocumentCollection<T> GetCollection<T>() where T : BaseDocument;
     IDocumentCollection<T> GetCollection<T>(string name) where T : BaseDocument;
