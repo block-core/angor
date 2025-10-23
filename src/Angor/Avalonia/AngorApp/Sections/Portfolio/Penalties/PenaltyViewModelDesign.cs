1ec65fb44b4f3c6b4ba08b6b8d42cec77983631f
José Manuel Nieto <superjmn@outlook.com>
2025-10-23 13:06:04 +0200

    Merge pull request #534
    * refactor: replace ProjectIdentifier with ProjectName in penalties-rel…

--- src/Angor/Avalonia/AngorApp/Sections/Portfolio/Penalties/PenaltyViewModelDesign.cs
+++ src/Angor/Avalonia/AngorApp/Sections/Portfolio/Penalties/PenaltyViewModelDesign.cs
@@ -5,7 +5,7 @@ public class PenaltyViewModelDesign : IPenaltyViewModel
     public IAmountUI TotalAmount { get; } = new AmountUI(123456789);
     public bool IsExpired { get; } = false;
     public IAmountUI AmountInRecovery { get; } = new AmountUI(244124);
-    public string ProjectIdentifier { get; } = "test-project-identifier";
+    public string ProjectName { get; } = "Project Name";
     public int DaysLeftForPenalty { get; } = 98;
     public string InvestorPubKey { get; } = "test-investor-pubkey";
 }
