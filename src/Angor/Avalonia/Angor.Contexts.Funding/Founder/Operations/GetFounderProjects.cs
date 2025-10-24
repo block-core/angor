5eb923846564c69871baf5c83b82cd4f3a7bd1ac
david <DavidGershony@gmail.com>
2025-10-24 17:14:03 +0100

    Converted the key derivation to store to db

--- src/Angor/Avalonia/Angor.Contexts.Funding/Founder/Operations/GetFounderProjects.cs
+++ src/Angor/Avalonia/Angor.Contexts.Funding/Founder/Operations/GetFounderProjects.cs
@@ -1,9 +1,9 @@
 using Angor.Contests.CrossCutting;
 using Angor.Contexts.Funding.Projects.Application.Dtos;
-using Angor.Contexts.Funding.Projects.Domain;
 using Angor.Contexts.Funding.Projects.Infrastructure.Impl;
 using Angor.Contexts.Funding.Services;
 using Angor.Contexts.Funding.Shared;
+using Angor.Data.Documents.Interfaces;
 using Angor.Shared;
 using CSharpFunctionalExtensions;
 using MediatR;
@@ -18,7 +18,7 @@ public static class GetFounderProjects
         ISeedwordsProvider seedwordsProvider,
         IDerivationOperations derivationOperations, 
         INetworkConfiguration networkConfiguration,
-        IStore store) : IRequestHandler<GetFounderProjectsRequest, Result<IEnumerable<ProjectDto>>>
+        IGenericDocumentCollection<DerivedProjectKeys> derivedProjectKeysCollection) : IRequestHandler<GetFounderProjectsRequest, Result<IEnumerable<ProjectDto>>>
     {
         public Task<Result<IEnumerable<ProjectDto>>> Handle(GetFounderProjectsRequest request, CancellationToken cancellationToken)
         {
@@ -30,14 +30,36 @@ public static class GetFounderProjects
 
         private async Task<Result<IEnumerable<ProjectId>>> GetProjectIds(GetFounderProjectsRequest request)
         {
-            var result = await seedwordsProvider.GetSensitiveData(request.WalletId)
-                .Map(p => p.ToWalletWords())
-                .Map(words => derivationOperations.DeriveProjectKeys(words, networkConfiguration.GetAngorKey()))//TODO we need to change this, the derivation code requires very heavy computations
-                .Map(collection => collection.Keys.AsEnumerable())
-                .MapEach(keys => keys.ProjectIdentifier)
-                .MapEach(fk => new ProjectId(fk));
+            // Try to get from storage first
+            var storageResult = await derivedProjectKeysCollection.FindByIdAsync(request.WalletId.ToString());
             
-            return result;
+            if (storageResult.IsSuccess && storageResult.Value != null)
+            {
+                // Return cached project identifiers from stored keys
+                return Result.Success(storageResult.Value.Keys.Select(k => new ProjectId(k.ProjectIdentifier)));
+            }
+
+            // If not in storage, derive the keys (expensive operation)
+            var keysResult = await seedwordsProvider.GetSensitiveData(request.WalletId)
+                .Map(p => p.ToWalletWords())
+                .Map(words => derivationOperations.DeriveProjectKeys(words, networkConfiguration.GetAngorKey()));
+            
+            if (keysResult.IsFailure)
+                return Result.Failure<IEnumerable<ProjectId>>(keysResult.Error);
+
+            // Store complete key sets in storage for future use
+            var founderKeys = keysResult.Value.Keys.ToList();
+
+            var derivedKeys = new DerivedProjectKeys
+            {
+                WalletId = request.WalletId.ToString(),
+                Keys = founderKeys
+            };
+            
+            await derivedProjectKeysCollection.UpsertAsync(x => x.WalletId, derivedKeys);
+
+            // Return project identifiers
+            return Result.Success(founderKeys.Select(k => new ProjectId(k.ProjectIdentifier)));
         }
     }
 
