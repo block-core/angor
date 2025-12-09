0ecf4d7f4ee3cc60ab4229d84ac37009aed206a1
david <DavidGershony@gmail.com>
2025-12-09 16:07:19 +0000

    Removed encryption files

--- src/Angor/Avalonia/Angor.Contexts.CrossCutting/IEncryptionKeyStore.cs
+++
@@ -1,8 +0,0 @@
-namespace Angor.Contexts.CrossCutting;
-
-public interface IEncryptionKeyStore
-{
-    Task<string?> GetKeyAsync(WalletId walletId);
-    Task SaveKeyAsync(WalletId walletId, string encryptionKey);
-    Task DeleteKeyAsync(WalletId walletId);
-}
