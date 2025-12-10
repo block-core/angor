4862482ea492f8ae3ed39519c7d356c8c2ae3c41
Kyle McCullen <kmccullen@protonmail.com>
2025-12-10 08:26:25 -0500

    add branta verification to send side

--- src/Angor/Avalonia/AngorApp/UI/Flows/SendWalletMoney/AddressAndAmount/AddressAndAmountViewModel.cs
+++ src/Angor/Avalonia/AngorApp/UI/Flows/SendWalletMoney/AddressAndAmount/AddressAndAmountViewModel.cs
@@ -1,3 +1,5 @@
+using Branta.V2.Classes;
+using Branta.V2.Models;
 using ReactiveUI.Validation.Extensions;
 using ReactiveUI.Validation.Helpers;
 
@@ -7,14 +9,22 @@ public partial class AddressAndAmountViewModel : ReactiveValidationObject, IAddr
 {
     [Reactive] private string? address;
     [Reactive] private long? amount;
+    [Reactive] private bool brantaLoading = false;
+    [Reactive] public bool showBrantaVerification = false;
+    [Reactive] public bool brantaPaymentsFound = false;
+    [Reactive] public List<Payment>? brantaPayments = [];
     [ObservableAsProperty] private IAmountUI? walletBalance;
 
+    private readonly BrantaClient _brantaClient;
 
-    public AddressAndAmountViewModel(IWallet wallet)
+
+    public AddressAndAmountViewModel(IWallet wallet, BrantaClient brantaClient)
     {
+        _brantaClient = brantaClient;
+
         walletBalanceHelper = wallet.WhenAnyValue(w => w.Balance)
             .ToProperty(this, model => model.WalletBalance);
-        
+
         this.ValidationRule(x => x.Amount, x => x is null or > 0, _ => "Amount must be greater than zero");
         this.ValidationRule(x => x.Amount, x => x is not null, _ => "Please, specify an amount");
         var isValidAmount = this.WhenAnyValue(x => x.Amount, x => x.WalletBalance, (amount, balance) => amount is null || amount <= balance.Sats);
@@ -22,7 +32,32 @@ public partial class AddressAndAmountViewModel : ReactiveValidationObject, IAddr
 
         this.ValidationRule(x => x.Address, x => !string.IsNullOrWhiteSpace(x), _ => "Please, specify an address");
         this.ValidationRule(x => x.Address, x => string.IsNullOrWhiteSpace(x) || wallet.IsAddressValid(x).IsSuccess, message => wallet.IsAddressValid(message).Error);
+
+        this.WhenAnyValue(x => x.Address)
+            .Subscribe(async newAddress => {
+                await OnAddressChangedAsync(newAddress);
+            });
     }
 
     public IObservable<bool> IsValid => this.IsValid();
+
+    private async Task OnAddressChangedAsync(string? newAddress)
+    {
+        BrantaLoading = true;
+
+        if (string.IsNullOrEmpty(newAddress)) {
+            BrantaPayments = null;
+            ShowBrantaVerification = false;
+            BrantaLoading = false;
+            return;
+        }
+
+        var payments = await _brantaClient.GetPaymentsAsync(newAddress);
+
+        BrantaPayments = payments;
+        BrantaPaymentsFound = payments.Count != 0;
+        ShowBrantaVerification = true;
+
+        BrantaLoading = false;
+    }
 }
--- src/Angor/Avalonia/AngorApp/UI/Flows/SendWalletMoney/SendMoneyFlow.cs
+++ src/Angor/Avalonia/AngorApp/UI/Flows/SendWalletMoney/SendMoneyFlow.cs
@@ -1,6 +1,7 @@
 using Angor.Contexts.Wallet.Application;
 using AngorApp.Model.Contracts.Flows;
 using AngorApp.UI.Shared.Controls.Common.Success;
+using Branta.V2.Classes;
 using Zafiro.Avalonia.Dialogs.Wizards.Slim;
 using Zafiro.UI.Wizards.Slim.Builder;
 using AddressAndAmountViewModel = AngorApp.UI.Flows.SendWalletMoney.AddressAndAmount.AddressAndAmountViewModel;
@@ -8,12 +9,12 @@ using TransactionDraftViewModel = AngorApp.UI.Flows.SendWalletMoney.TransactionD
 
 namespace AngorApp.UI.Flows.SendWalletMoney;
 
-public class SendMoneyFlow(IWalletAppService walletAppService, UIServices uiServices) : ISendMoneyFlow
+public class SendMoneyFlow(IWalletAppService walletAppService, UIServices uiServices, BrantaClient brantaClient) : ISendMoneyFlow
 {
     public async Task SendMoney(IWallet sourceWallet)
     {
         var wizard = WizardBuilder
-            .StartWith(() => new AddressAndAmountViewModel(sourceWallet), "Amount and address").Next(model => (model.Amount, model.Address)).WhenValid<AddressAndAmountViewModel>()
+            .StartWith(() => new AddressAndAmountViewModel(sourceWallet, brantaClient), "Amount and address").Next(model => (model.Amount, model.Address)).WhenValid<AddressAndAmountViewModel>()
             .Then(sendData => new TransactionDraftViewModel(sourceWallet.Id, walletAppService, new SendAmount("Test", sendData.Amount.Value, sendData.Address), uiServices), "Summary").NextCommand(model => model.Confirm.Enhance("Confirm"))
             .Then(_ => new SuccessViewModel("Transaction sent!"), "Transaction sent").NextUnit("Close").Always()
             .WithCompletionFinalStep();
