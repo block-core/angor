d52e59045979a613a7432ff9c5dc89044fc0eb1c
Dan Gershony <dan.gershony@gmail.com>
2026-03-16 14:52:04 +0000

    Fix a wallet bug (#702)

---
+++ src/Angor/Avalonia/Angor.Sdk.Tests/Common/WalletAccountBalanceServiceTests.cs
@@ -0,0 +1,68 @@
+using Angor.Sdk.Common;
+using Angor.Shared;
+using Angor.Shared.Models;
+using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Angor.Data.Documents.Interfaces;
+using Xunit;
+
+namespace Angor.Sdk.Tests.Common;
+
+public class WalletAccountBalanceServiceTests
+{
+    [Fact]
+    public async Task RefreshAccountBalanceInfoAsync_removes_stale_pending_receive_utxos()
+    {
+        var walletId = new WalletId("wallet-1");
+        var accountInfo = new AccountInfo
+        {
+            walletId = walletId.Value,
+            ExtPubKey = "ext",
+            RootExtPubKey = "root",
+            Path = "m/84'/1'/0'"
+        };
+
+        accountInfo.AddressesInfo.Add(new AddressInfo { Address = "tb1qtest" });
+
+        var stalePending = new UtxoData
+        {
+            address = "tb1qtest",
+            scriptHex = "0014",
+            outpoint = new Outpoint("stale-tx", 0),
+            value = 50_000,
+            blockIndex = 0
+        };
+
+        var balanceInfo = new AccountBalanceInfo();
+        balanceInfo.UpdateAccountBalanceInfo(accountInfo, [stalePending]);
+
+        var walletOperations = new Mock<IWalletOperations>();
+        walletOperations
+            .Setup(x => x.UpdateDataForExistingAddressesAsync(accountInfo))
+            .Returns(Task.CompletedTask);
+        walletOperations
+            .Setup(x => x.UpdateAccountInfoWithNewAddressesAsync(accountInfo))
+            .Returns(Task.CompletedTask);
+
+        var collection = new Mock<IGenericDocumentCollection<WalletAccountBalanceInfo>>();
+        collection
+            .Setup(x => x.FindByIdAsync(walletId.Value))
+            .ReturnsAsync(Result.Success<WalletAccountBalanceInfo?>(new WalletAccountBalanceInfo
+            {
+                WalletId = walletId.Value,
+                AccountBalanceInfo = balanceInfo
+            }));
+        collection
+            .Setup(x => x.UpsertAsync(It.IsAny<System.Linq.Expressions.Expression<System.Func<WalletAccountBalanceInfo, string>>>(), It.IsAny<WalletAccountBalanceInfo>()))
+            .ReturnsAsync(Result.Success(true));
+
+        var sut = new WalletAccountBalanceService(walletOperations.Object, collection.Object, NullLogger<WalletAccountBalanceService>.Instance);
+
+        var result = await sut.RefreshAccountBalanceInfoAsync(walletId);
+
+        Assert.True(result.IsSuccess);
+        Assert.Empty(result.Value.AccountPendingReceive);
+        Assert.Equal(0, result.Value.TotalUnconfirmedBalance);
+    }
+}
