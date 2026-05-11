d6405a66619556138766ecdcc58d4cca95e28a9f
Dan Gershony <dan.gershony@gmail.com>
2026-05-11 22:58:55 +0100

    chore: remove Cypress and legacy e2e tests (resolves npm security alerts) (#840)

--- src/Testing/cypress.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  e2e: {
-    specPattern: "src/Testing/e2e/*.js",
-    supportFile: false, // Disable support file
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-};
